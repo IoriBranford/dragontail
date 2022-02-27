d7190ccfb9431660685b82d757c99d55cd545155
IoriBranford <ioribranford@gmail.com>
2022-02-27 15:41:38 -0800

    Start a map.

---
+++ data/tilesets/fence.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="fence" tilewidth="32" tileheight="32" tilecount="336" columns="24">
+ <image source="fence.png" width="768" height="448"/>
+</tileset>
---
+++ data/tilesets/village_houses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="village_houses" tilewidth="32" tileheight="32" tilecount="720" columns="30">
+ <image source="village_houses.png" width="960" height="768"/>
+</tileset>
