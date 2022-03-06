05b9780205d4360580d1bf39e1361ae3861911bb
IoriBranford <ioribranford@gmail.com>
2022-03-06 13:55:37 -0800

    Update unit tests.

--- test/Dragontail/Character_tests.lua
+++ test/Dragontail/Character_tests.lua
@@ -1,6 +1,7 @@
 local cute = require("cute")
 
 local Character = require "Dragontail.Character"
+require "Dragontail.Character.Ai"
 
 notion("Character moving on XY plane", function()
     local character = Character.new({x = 0, y = 0})
@@ -33,22 +34,22 @@ end)
 
 notion("Character keeping out of other's body", function()
     local a = Character.new({x = 0, y = 0, bodyradius = 3})
-    local b = Character.new({x = 16, y = 0, bodyradius = 2})
+    local b = Character.new({x = 16, y = 0, bodyradius = 2, bodysolid = true})
     a:move(15, 0)
     a:collideWithCharacterBody(b)
     check(a.x).is(11)
 end)
 
 notion("Character getting damaged by other's attack", function()
-    local a = Character.new({x = 0, y = 0, health = 2, bodyradius = 2})
-    local b = Character.new({x = 4, y = 0, attackradius = 4, attackangle = math.pi, attackarc = math.pi/2, attackdamage = 1})
+    local a = Character.new({x = 0, y = 0, health = 2, bodyradius = 2, bodysolid = true})
+    local b = Character.new({x = 4, y = 0, attackradius = 4, attackangle = math.pi, attackarc = math.pi/4, attackdamage = 1})
     a:collideWithCharacterAttack(b)
     check(a.health).is(1)
 end)
 
 notion("Character not damaged during hurtstun", function()
-    local a = Character.new({x = 0, y = 0, health = 2, bodyradius = 2})
-    local b = Character.new({x = 4, y = 0, attackradius = 4, attackangle = math.pi, attackarc = math.pi/2, attackdamage = 1, attackstun = 1})
+    local a = Character.new({x = 0, y = 0, health = 2, bodyradius = 2, bodysolid = true})
+    local b = Character.new({x = 4, y = 0, attackradius = 4, attackangle = math.pi, attackarc = math.pi/4, attackdamage = 1, attackstun = 1})
     a:collideWithCharacterAttack(b)
     a:collideWithCharacterAttack(b)
     check(a.health).is(1)
@@ -56,14 +57,14 @@ end)
 
 notion("Character not damaged by attack in the wrong direction", function()
     local a = Character.new({x = 0, y = 0, health = 2, bodyradius = 2})
-    local b = Character.new({x = 4, y = 0, attackradius = 4, attackangle = math.pi/2, attackarc = math.pi/2, attackdamage = 1})
+    local b = Character.new({x = 4, y = 0, attackradius = 4, attackangle = math.pi/2, attackarc = math.pi/4, attackdamage = 1})
     a:collideWithCharacterAttack(b)
     check(a.health).is(2)
 end)
 
 notion("Character not damaged by out-of-range attack", function()
     local a = Character.new({x = 0, y = 0, health = 2, bodyradius = 2})
-    local b = Character.new({x = 4, y = 0, attackradius = 2, attackangle = math.pi, attackarc = math.pi/2, attackdamage = 1})
+    local b = Character.new({x = 4, y = 0, attackradius = 2, attackangle = math.pi, attackarc = math.pi/4, attackdamage = 1})
     a:collideWithCharacterAttack(b)
     check(a.health).is(2)
 end)
