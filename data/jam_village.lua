5fa9497ea3cd5a62b38e6d52648bf61214b62b57
IoriBranford <ioribranford@gmail.com>
2022-03-10 18:22:44 -0800

    Fix extra fish and box spawning in fight 6.

--- data/jam_village.lua
+++ data/jam_village.lua
@@ -4203,36 +4203,6 @@ return {
           properties = {
             ["startai"] = "approach"
           }
-        },
-        {
-          id = 77,
-          name = "",
-          type = "food-bigfish",
-          shape = "point",
-          x = 2240,
-          y = 145,
-          width = 0,
-          height = 0,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["z"] = 16
-          }
-        },
-        {
-          id = 78,
-          name = "",
-          type = "food-container",
-          shape = "point",
-          x = 2240,
-          y = 144,
-          width = 0,
-          height = 0,
-          rotation = 0,
-          visible = true,
-          properties = {
-            ["item"] = { id = 77 }
-          }
         }
       }
     },
