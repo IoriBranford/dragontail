30f75cee862078a9256d1519fb69e51bc6b9cb08
IoriBranford <ioribranford@gmail.com>
2022-03-13 01:26:27 -0800

    Support multiple csv files adding values to the same database row.

--- scripts/Data/Database.lua
+++ scripts/Data/Database.lua
@@ -3,38 +3,45 @@ local Audio = require "System.Audio"
 local Csv   = require "Data.Csv"
 local type = type
 
-local sheets = {}
+local database = {}
 
 function Database.load(csvfilename)
-    local loaded = Csv.load(csvfilename)
-    local fieldnames = loaded[1]
+    local loadedrows = Csv.load(csvfilename)
+    local fieldnames = loadedrows[1]
     for i = 1, #fieldnames do
         fieldnames[i] = fieldnames[i]:match("^([_A-Za-z]+)")
     end
-    for i = 2, #loaded do
-        local t = loaded[i]
-        local typename = t[1]
-        sheets[typename] = t
-        loaded[typename] = t
-        for i = #t, 1, -1 do
-            if t[i] ~= "" then
-                t[fieldnames[i]] = t[i]
+    for i = 2, #loadedrows do
+        local row = loadedrows[i]
+        local key = row[1]
+        loadedrows[key] = row
+        for i = #row, 1, -1 do
+            if row[i] ~= "" then
+                row[fieldnames[i]] = row[i]
             end
-            t[i] = nil
+            row[i] = nil
+        end
+        local existingrow = database[key]
+        if existingrow then
+            for k,v in pairs(row) do
+                existingrow[k] = v
+            end
+        else
+            database[key] = row
         end
     end
-    for i = #loaded, 1, -1 do
-        loaded[i] = nil
+    for i = #loadedrows, 1, -1 do
+        loadedrows[i] = nil
     end
-    return loaded
+    return loadedrows
 end
 
 function Database.get(key)
-    return sheets[key]
+    return database[key]
 end
 
 function Database.clear()
-    sheets = {}
+    database = {}
 end
 
 local function set(unit, k, v)
@@ -43,9 +50,9 @@ local function set(unit, k, v)
 end
 
 function Database.fillBlanks(unit, key)
-    local sheet = type(key) == "table" and key or sheets[key]
-    if sheet then
-        for k,v in pairs(sheet) do
+    local row = type(key) == "table" and key or database[key]
+    if row then
+        for k,v in pairs(row) do
             if unit[k] == nil then
                 set(unit, k, v)
             end
@@ -54,13 +61,13 @@ function Database.fillBlanks(unit, key)
 end
 
 function Database.fill(unit, key)
-    local sheet = type(key) == "table" and key or sheets[key]
-    if sheet then
-        for k,v in pairs(sheet) do
+    local row = type(key) == "table" and key or database[key]
+    if row then
+        for k,v in pairs(row) do
             set(unit, k, v)
         end
 
-        local think = unit[sheet.think]
+        local think = unit[row.think]
         if type(think) == "function" then
             unit.think = think
         end
@@ -70,7 +77,7 @@ function Database.fill(unit, key)
 end
 
 function Database.forEach(func)
-    for name, properties in pairs(sheets) do
+    for name, properties in pairs(database) do
         func(name, properties)
     end
 end
