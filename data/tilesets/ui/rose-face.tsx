964cf6be6b63e5ed390b83e2aa250309748a9290
IoriBranford <ioribranford@gmail.com>
2022-11-01 20:44:36 -0700

    Make Rose portraits a tileset.

---
+++ data/tilesets/ui/rose-face.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="rose-face" tilewidth="32" tileheight="32" tilecount="4" columns="4" objectalignment="center">
+ <image source="rose-face.png" width="128" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="normal"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="hurt"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="attack"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="win"/>
+  </properties>
+ </tile>
+</tileset>
