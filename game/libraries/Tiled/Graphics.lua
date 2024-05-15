375b1e6d40586a8f1296c1bd0f243b39130e7aee
IoriBranford <ioribranford@gmail.com>
2024-05-14 19:17:12 -0700

    Support bit flags to exclude values in pushTransform.

--- game/libraries/Tiled/Graphics.lua
+++ game/libraries/Tiled/Graphics.lua
@@ -5,14 +5,26 @@ local _transform = love.math.newTransform()
 local colorstack = {}
 local red, green, blue, alpha = 1, 1, 1, 1
 
-function Graphics.pushTransform(element)
-    love.graphics.push()
-    _transform:setTransformation(
+function Graphics.pushTransform(element, excludeflags)
+    local x, y, r, sx, sy, ox, oy, kx, ky =
         element.x, element.y,
         element.rotation or 0,
         element.scalex or 1, element.scaley or 1,
         element.originx or 0, element.originy or 0,
-        element.skewx or 0, element.skewy or 0)
+        element.skewx or 0, element.skewy or 0
+    if excludeflags then
+        if bit.band(excludeflags, 1) then x = 0 end
+        if bit.band(excludeflags, 2) then y = 0 end
+        if bit.band(excludeflags, 4) then r = 0 end
+        if bit.band(excludeflags, 8) then sx = 1 end
+        if bit.band(excludeflags, 16) then sy = 1 end
+        if bit.band(excludeflags, 32) then ox = 0 end
+        if bit.band(excludeflags, 64) then oy = 0 end
+        if bit.band(excludeflags, 128) then kx = 0 end
+        if bit.band(excludeflags, 256) then ky = 0 end
+    end
+    love.graphics.push()
+    _transform:setTransformation(x, y, r, sx, sy, ox, oy, kx, ky)
     love.graphics.applyTransform(_transform)
 end
 
