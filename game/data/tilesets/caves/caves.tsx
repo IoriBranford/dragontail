7bec8e06174bde9ac8fc76fa157888aa9f50b036
IoriBranford <ioribranford@gmail.com>
2024-08-21 21:18:19 -0700

    Add a cave tileset from Szadi art.

---
+++ game/data/tilesets/caves/caves.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="caves" tilewidth="32" tileheight="32" tilecount="2448" columns="51">
+ <image source="caves.png" width="1632" height="1536"/>
+</tileset>
---
+++ game/data/tilesets/caves/caves_decorative.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="caves_decorative" tilewidth="32" tileheight="32" tilecount="390" columns="13">
+ <image source="caves_decorative.png" width="416" height="960"/>
+</tileset>
