0ceae096a861a0ee70aa0322bc0f59ddbaac5cda
IoriBranford <ioribranford@gmail.com>
2024-08-26 22:01:04 -0700

    Bandit tile objects.

---
+++ game/data/objects/bandit/bandits.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="bandits" tilewidth="64" tileheight="64" tilecount="16" columns="16" objectalignment="bottom">
+ <tileoffset x="0" y="8"/>
+ <image source="bandits.png" width="1024" height="64"/>
+ <tile id="0" type="bandit-dagger"/>
+ <tile id="1" type="bandit-dagger"/>
+ <tile id="2" type="bandit-dagger"/>
+ <tile id="3" type="bandit-dagger"/>
+ <tile id="4" type="bandit-spear"/>
+ <tile id="5" type="bandit-spear"/>
+ <tile id="6" type="bandit-spear"/>
+ <tile id="7" type="bandit-spear"/>
+ <tile id="8" type="bandit-bow"/>
+ <tile id="9" type="bandit-bow"/>
+ <tile id="10" type="bandit-bow"/>
+ <tile id="11" type="bandit-bow"/>
+ <tile id="12" type="bandit-arrow"/>
+ <tile id="13" type="bandit-arrow"/>
+ <tile id="14" type="bandit-arrow"/>
+ <tile id="15" type="bandit-arrow"/>
+</tileset>
