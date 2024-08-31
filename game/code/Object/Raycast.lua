f163766a32285cdc9abcf088183522c0fe9213c2
IoriBranford <ioribranford@gmail.com>
2024-08-31 01:57:16 -0700

    Raycast radius can be passed.

--- game/code/Object/Raycast.lua
+++ game/code/Object/Raycast.lua
@@ -1,11 +1,11 @@
 ---@class Raycast
 local Raycast = class()
 
-function Raycast:_init(dx, dy, canhitside)
+function Raycast:_init(dx, dy, canhitside, radius)
     self.dx = dx or 1
     self.dy = dy or 0
     self.canhitside = canhitside or 0 --- negative = only outsides, positive = only insides, 0 = both sides
-    self.radius = 0
+    self.radius = radius or 0
     self.hitx = nil ---@type number?
     self.hity = nil ---@type number?
     self.hitdist = nil ---@type number?
