0b09400e834c0ec23baf44f2faa5b8ba0bd7d78d
IoriBranford <ioribranford@gmail.com>
2024-09-04 14:26:03 -0700

    Add throwing axe data along with new item sprites and map objects.

---
+++ game/data/mapobjects/items.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="items" tilewidth="64" tileheight="32" tilecount="3" columns="3" objectalignment="center">
+ <image source="items.png" width="192" height="32"/>
+ <tile id="0" type="food-fish"/>
+ <tile id="1" type="food-bigfish"/>
+ <tile id="2" type="item-throwing-axe"/>
+</tileset>
