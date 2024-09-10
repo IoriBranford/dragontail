b7a5a78249e75b35df50063bdcf8f4fc3d8c590a
IoriBranford <ioribranford@gmail.com>
2024-09-09 21:32:37 -0700

    Stone sprites.

--- game/data/mapobjects/items.tsx
+++ game/data/mapobjects/items.tsx
@@ -1,7 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.0" name="items" tilewidth="64" tileheight="32" tilecount="3" columns="3" objectalignment="center">
- <image source="items.png" width="192" height="32"/>
+<tileset version="1.11" tiledversion="1.11.0" name="items" tilewidth="64" tileheight="32" tilecount="4" columns="4" objectalignment="center">
+ <image source="items.png" width="256" height="32"/>
  <tile id="0" type="food-fish"/>
  <tile id="1" type="food-bigfish"/>
  <tile id="2" type="item-throwing-axe"/>
+ <tile id="3" type="item-stone"/>
 </tileset>
