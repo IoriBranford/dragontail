6a8d2b4c42303aa3d7a8a1e82930a3749010bb1a
IoriBranford <ioribranford@gmail.com>
2024-09-23 13:19:22 -0700

    Set HUD portrait pivot to bottom.

--- game/data/tilesets/ui/rose-face.tsx
+++ game/data/tilesets/ui/rose-face.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="rose-face" tilewidth="32" tileheight="32" tilecount="4" columns="4" objectalignment="center">
+<tileset version="1.11" tiledversion="1.11.0" name="rose-face" tilewidth="32" tileheight="32" tilecount="4" columns="4" objectalignment="bottom">
  <image source="rose-face.png" width="128" height="32"/>
  <tile id="0">
   <properties>
