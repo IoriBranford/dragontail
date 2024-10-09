a1546e5d11e4bce672dc317d15ddd45dcd08b110
IoriBranford <ioribranford@gmail.com>
2024-10-08 17:02:52 -0700

    Add sprite for darkness inside diagonal door.

--- game/data/sprites/bandit/cavedoor2-diagonal.tsx
+++ game/data/sprites/bandit/cavedoor2-diagonal.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.0" name="cavedoor2-diagonal" tilewidth="64" tileheight="128" tilecount="3" columns="3" objectalignment="topright">
- <image source="cavedoor2-diagonal.png" width="192" height="128"/>
+<tileset version="1.11" tiledversion="1.11.0" name="cavedoor2-diagonal" tilewidth="64" tileheight="128" tilecount="4" columns="4" objectalignment="topright">
+ <image source="cavedoor2-diagonal.png" width="256" height="128"/>
  <tile id="1" type="bandit-cave-door">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="64">
