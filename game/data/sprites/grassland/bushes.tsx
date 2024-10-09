1833286942132c19e256a2f74d4aa970dcdaa000
IoriBranford <ioribranford@gmail.com>
2024-10-08 17:01:45 -0700

    Bush decoration assets.

---
+++ game/data/sprites/grassland/bushes.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="bushes" tilewidth="48" tileheight="32" tilecount="16" columns="4" objectalignment="bottom">
+ <tileoffset x="0" y="8"/>
+ <image source="bushes.png" width="192" height="128"/>
+</tileset>
