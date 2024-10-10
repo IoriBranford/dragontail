9389c901eca2f1f7fc334b9668db7c2cd1510cb2
IoriBranford <ioribranford@gmail.com>
2024-10-10 14:18:45 -0700

    Two types of barrel sprites.

---
+++ game/data/sprites/containers/barrelA.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="barrelA" tilewidth="32" tileheight="32" tilecount="3" columns="3" objectalignment="bottom">
+ <tileoffset x="0" y="8"/>
+ <image source="barrelA.png" width="96" height="32"/>
+ <tile id="0" type="container-barrel"/>
+ <tile id="1" type="container-barrel">
+  <properties>
+   <property name="name" value="collapse"/>
+  </properties>
+ </tile>
+ <tile id="2" type="container-barrel"/>
+</tileset>
