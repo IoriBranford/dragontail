14dd7ef6cda0e7bf3620829eb697c9e533b7432c
IoriBranford <ioribranford@gmail.com>
2024-10-11 16:50:20 -0700

    Stools tileset.

---
+++ game/data/sprites/banditcave/stools.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="stools" tilewidth="32" tileheight="32" tilecount="3" columns="3" objectalignment="bottom">
+ <tileoffset x="0" y="8"/>
+ <image source="stools.png" width="96" height="32"/>
+ <tile id="0" type="container-stool"/>
+ <tile id="1" type="container-stool"/>
+ <tile id="2" type="container-stool"/>
+</tileset>
