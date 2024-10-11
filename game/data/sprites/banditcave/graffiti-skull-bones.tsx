68d701fba34ce4a4397e2b483b09799f24811376
IoriBranford <ioribranford@gmail.com>
2024-10-11 14:57:31 -0700

    Skull and bones graffiti sprites.

---
+++ game/data/sprites/banditcave/graffiti-skull-bones.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="graffiti-skull-bones" tilewidth="16" tileheight="16" tilecount="14" columns="14" objectalignment="center">
+ <image source="graffiti-skull-bones.png" width="224" height="16"/>
+</tileset>
