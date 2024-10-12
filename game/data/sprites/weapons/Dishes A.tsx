3842ecd2b49eb498dd1e91fe4aff78b3f7ccec67
IoriBranford <ioribranford@gmail.com>
2024-10-12 15:18:54 -0700

    Give dish tile a name.

--- game/data/sprites/weapons/Dishes A.tsx
+++ game/data/sprites/weapons/Dishes A.tsx
@@ -1,5 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.0" name="Dishes A" tilewidth="32" tileheight="32" tilecount="16" columns="8" objectalignment="bottom">
- <tileoffset x="0" y="8"/>
+<tileset version="1.11" tiledversion="1.11.0" name="Dishes A" tilewidth="32" tileheight="32" tilecount="16" columns="8" objectalignment="center">
  <image source="../banditcave/Dishes A.png" width="256" height="64"/>
+ <tile id="0" type="item-dish">
+  <properties>
+   <property name="name" value="dish-small"/>
+  </properties>
+ </tile>
 </tileset>
