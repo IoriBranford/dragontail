39f36df5ab6145b9f02cea471b695df15a560cbb
IoriBranford <ioribranford@gmail.com>
2024-10-16 21:34:05 +0800

    Collision volumes for more workbenches

--- game/data/sprites/banditcave/Workbench, Smith.tsx
+++ game/data/sprites/banditcave/Workbench, Smith.tsx
@@ -2,6 +2,26 @@
 <tileset version="1.11" tiledversion="1.11.0" name="Workbench, Smith" tilewidth="64" tileheight="64" tilecount="6" columns="3" objectalignment="bottom">
  <tileoffset x="0" y="8"/>
  <image source="Workbench, Smith.png" width="192" height="128"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="64">
+    <properties>
+     <property name="collidable" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 64,0 64,-32 0,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="64">
+    <properties>
+     <property name="collidable" type="bool" value="true"/>
+    </properties>
+    <polygon points="0,0 64,0 64,-32 0,-32"/>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="4">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="48" width="64" height="16">
@@ -13,12 +33,12 @@
   </objectgroup>
  </tile>
  <tile id="5">
-  <objectgroup>
+  <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="48" width="64" height="16">
     <properties>
      <property name="collidable" type="bool" value="true"/>
     </properties>
-    <polygon points="0,0 0,16 64,16 64,0"/>
+    <polygon points="0,16 16,16 64,0 48,0"/>
    </object>
   </objectgroup>
  </tile>
