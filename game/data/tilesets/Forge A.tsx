5dbe795b8c8fc6fcf9276501fd083b083024b997
IoriBranford <ioribranford@gmail.com>
2024-10-16 21:29:58 +0800

    Assets for forge that should burn anyone who goes in.

---
+++ game/data/tilesets/Forge A.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="Forge A" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <image source="Forge A.png" width="512" height="256"/>
+ <tile id="88">
+  <animation>
+   <frame tileid="88" duration="100"/>
+   <frame tileid="92" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="89">
+  <animation>
+   <frame tileid="89" duration="100"/>
+   <frame tileid="93" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="90">
+  <animation>
+   <frame tileid="90" duration="100"/>
+   <frame tileid="94" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="91">
+  <animation>
+   <frame tileid="91" duration="100"/>
+   <frame tileid="95" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="104">
+  <animation>
+   <frame tileid="104" duration="100"/>
+   <frame tileid="108" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="105">
+  <animation>
+   <frame tileid="105" duration="100"/>
+   <frame tileid="109" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="106">
+  <animation>
+   <frame tileid="106" duration="100"/>
+   <frame tileid="110" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="107">
+  <animation>
+   <frame tileid="107" duration="100"/>
+   <frame tileid="111" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
