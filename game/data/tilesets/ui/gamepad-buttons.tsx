3a45eebc9c052d0d620b67dadbf8b986010fbb2a
IoriBranford <ioribranford@gmail.com>
2024-11-08 18:30:36 -0800

    Gamepad button assets.

---
+++ game/data/tilesets/ui/gamepad-buttons.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="gamepad-buttons" tilewidth="16" tileheight="16" tilecount="60" columns="20">
+ <image source="gamepad-buttons.png" width="320" height="48"/>
+ <tile id="9">
+  <animation>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
