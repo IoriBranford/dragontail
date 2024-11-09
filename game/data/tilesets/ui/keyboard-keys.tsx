7402969aab9d23b05e9cb9da8dfab706e9a7ff1f
IoriBranford <ioribranford@gmail.com>
2024-11-09 14:17:10 -0800

    Keyboard key tileset from Kenney.

---
+++ game/data/tilesets/ui/keyboard-keys.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.0" name="keyboard-keys" tilewidth="16" tileheight="16" tilecount="272" columns="17">
+ <image source="keyboard-keys.png" width="272" height="256"/>
+ <tile id="206">
+  <animation>
+   <frame tileid="70" duration="100"/>
+   <frame tileid="206" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="207">
+  <animation>
+   <frame tileid="71" duration="100"/>
+   <frame tileid="207" duration="900"/>
+  </animation>
+ </tile>
+</tileset>
