0f73789ecb6da6714fede3609f4f3244512fbbd2
IoriBranford <ioribranford@gmail.com>
2024-12-09 19:56:07 -0800

    Start coming up with new attack data structure.

---
+++ game/code/Dragontail/Character/Attack.lua
@@ -0,0 +1,34 @@
+
+---@class Attack
+---
+---How is the attack placed in the world?
+---@field radius number
+---@field arc number
+---@field projectile string
+---
+---What happens to them on impact?
+---@field damage number
+---@field opponentstuntime integer
+---@field damagestate string
+---@field defeatstate string
+---@field damageparticle string
+---@field damagecolorcycle string
+---@field pushforce number
+---@field launchforce number
+---
+---What happens to me on impact?
+---@field selfstuntime integer
+---@field stateonhitopponent string
+---@field stateonhitwall string
+---@field stateonhitanything string
+---
+---What else happens on impact?
+---@field damagespark string
+---@field damagesound string
+---@field guardspark string
+---@field guardsound string
+---
+---Other properties
+---@field invulnerable boolean
+---@field candeflectprojectile boolean
+---@field canjuggle boolean
