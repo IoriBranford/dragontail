0d03b780af0754678872db83f97d3f6c095b8fd3
IoriBranford <ioribranford@gmail.com>
2025-02-20 15:31:26 -0800

    Flame sprite for full flame gauge.

---
+++ game/data/tilesets/ui/flamegaugefull.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="flamegaugefull" tilewidth="40" tileheight="20" tilecount="8" columns="1">
+ <image source="flamegaugefull.png" width="40" height="160"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
