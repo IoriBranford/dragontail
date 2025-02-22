78d7ca51da970da4f25645383d1cb74f2c6d6b4f
IoriBranford <ioribranford@gmail.com>
2025-02-21 20:54:35 -0800

    Face.lua

---
+++ game/code/Dragontail/Character/Action/Face.lua
@@ -0,0 +1,26 @@
+---@class Face:Character
+---@field faceangle number
+local Face = {}
+
+function Face:init()
+    self.faceangle = self.faceangle or 0
+end
+
+function Face:facePosition(x, y, animation, frame1, loopframe)
+    Face.faceVector(self, x - self.x, y - self.y, animation, frame1, loopframe)
+end
+
+function Face:faceVector(vx, vy, animation, frame1, loopframe)
+    Face.faceAngle(self, (vx ~= 0 or vy ~= 0) and math.atan2(vy, vx), animation, frame1, loopframe)
+end
+
+function Face:faceAngle(angle, animation, frame1, loopframe)
+    if angle then
+        self.faceangle = angle
+    end
+    if animation then
+        self:setDirectionalAnimation(animation, angle or self.faceangle, frame1, loopframe)
+    end
+end
+
+return Face
