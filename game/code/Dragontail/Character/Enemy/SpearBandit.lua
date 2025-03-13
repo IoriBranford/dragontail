53153a4566919cc4523906e7c44a408b1aef2742
IoriBranford <ioribranford@gmail.com>
2025-03-13 16:18:57 -0700

    SpearBandit.lua
    Adds poke attack after a number of dodges and restores dodge during attack windup

---
+++ game/code/Dragontail/Character/Enemy/SpearBandit.lua
@@ -0,0 +1,85 @@
+local Enemy = require "Dragontail.Character.Enemy"
+local Face       = require "Dragontail.Character.Action.Face"
+local Dodge = require "Dragontail.Character.Action.Dodge"
+local Database = require "Data.Database"
+
+---@class SpearBandit:Enemy
+---@field numdodges integer
+local SpearBandit = class(Enemy)
+
+function SpearBandit:duringStand()
+    local opponent = self.opponents[1]
+    Face.facePosition(self, opponent.x, opponent.y, "Stand")
+    -- local oppox, oppoy = opponent.x, opponent.y
+    -- local tooppox, tooppoy = oppox - self.x, oppoy - self.y
+    -- local seesopponent = math.dot(math.cos(self.faceangle), math.sin(self.faceangle), tooppox, tooppoy) >= 0
+    local dodgeangle = self:isFullyOnCamera(self.camera) and Dodge.findDodgeAngle(self)
+    if dodgeangle then
+        self.numdodges = (self.numdodges or 0) + 1
+        return "dodgeIncoming", dodgeangle
+    end
+end
+
+function SpearBandit:afterStand()
+    return Enemy.afterStand(self)
+end
+
+function SpearBandit:duringApproach(target)
+    -- local oppox, oppoy = opponent.x, opponent.y
+    -- local tooppox, tooppoy = oppox - self.x, oppoy - self.y
+    -- local seesopponent = math.dot(math.cos(self.faceangle), math.sin(self.faceangle), tooppox, tooppoy) >= 0
+    local dodgeangle = self:isFullyOnCamera(self.camera) and Dodge.findDodgeAngle(self)
+    if dodgeangle then
+        self.numdodges = (self.numdodges or 0) + 1
+        return "dodgeIncoming", dodgeangle
+    end
+end
+
+function SpearBandit:duringDodge()
+    if (self.numdodges or 0) >= 2 then
+        if math.abs(self.velx) < 1 and math.abs(self.vely) < 1 then
+            local attacktype = "spear-poke"
+            local attackdata = Database.get(attacktype)
+            local opponent = self.opponents[1]
+            local attackradius = opponent.bodyradius + Enemy.TotalAttackRange(
+                attackdata and attackdata.attackradius or 32,
+                attackdata and attackdata.attacklungespeed or 0,
+                attackdata and attackdata.attacklungedecel or 1)
+
+            if math.distsq(self.x, self.y, opponent.x, opponent.y) <= attackradius*attackradius then
+                return "attack", attacktype
+            end
+        end
+    end
+end
+
+function SpearBandit:decideNextAttack()
+    return Enemy.decideNextAttack(self)
+end
+
+function SpearBandit:duringPrepareAttack(target)
+    if (self.numdodges or 0) < 2 then
+        local dodgeangle = self:isFullyOnCamera(self.camera) and Dodge.findDodgeAngle(self)
+        if dodgeangle then
+            self.numdodges = (self.numdodges or 0) + 1
+            self.color = self:getAttackFlashColor(0)
+            return "dodgeIncoming", dodgeangle
+        end
+    end
+    return Enemy.duringPrepareAttack(self, target)
+end
+
+function SpearBandit:duringAttackSwing(target)
+    self.numdodges = 0
+    return Enemy.duringAttackSwing(self, target)
+end
+
+function SpearBandit:beforeGetUp(attacker)
+    return Enemy.beforeGetUp(self, attacker)
+end
+
+function SpearBandit:duringGetUp(attacker)
+    return Enemy.duringGetUp(self, attacker)
+end
+
+return SpearBandit
