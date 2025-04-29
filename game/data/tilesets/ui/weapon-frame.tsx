3a9c42042bac213b787b0ac11499aa1471537e32
IoriBranford <ioribranford@gmail.com>
2025-04-29 16:13:38 -0700

    Weapon slot frame graphics

---
+++ game/data/tilesets/ui/weapon-frame.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="weapon-frame" tilewidth="24" tileheight="24" tilecount="3" columns="3" objectalignment="center">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="weapon-frame.png" width="72" height="24"/>
+</tileset>
