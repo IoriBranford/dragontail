31769109d12aa633f779206c9b3bbec907d726ea
IoriBranford <ioribranford@gmail.com>
2025-04-29 04:03:40 -0700

    On save, map can mark all tilesets' empty tiles and remove them from its tile layers.

--- game/data/extensions/tile-inspect/tile-inspect.js
+++ game/data/extensions/tile-inspect/tile-inspect.js
@@ -5,41 +5,82 @@
  * @param {Tileset} tileset 
  */
 
-function markAndCountEmptyTiles(tileset) {
-    let numEmpty = tileset.tileCount
-    tileset.tiles.map(tile => {
-        let empty = true
-        let image = tile.image
-        let rect = tile.imageRect
-        let x2 = rect.x + rect.width
-        let y2 = rect.y + rect.height
-        for (let y = rect.y; empty && y < y2; ++y) {
-            for (let x = rect.x; empty && x < x2; ++x) {
-                if (image.pixel(x, y) >= 0x01000000) {
-                    empty = false
-                    --numEmpty
+function Tileset_updateEmptyTiles(tileset) {
+    tileset.macro("Update empty tiles", () => {
+        let numEmpty = tileset.tileCount
+        tileset.tiles.forEach(tile => {
+            let empty = true
+            let image = tile.image
+            let rect = tile.imageRect
+            let x2 = rect.x + rect.width
+            let y2 = rect.y + rect.height
+            for (let y = rect.y; empty && y < y2; ++y) {
+                for (let x = rect.x; empty && x < x2; ++x) {
+                    if (image.pixel(x, y) >= 0x01000000) {
+                        empty = false
+                        --numEmpty
+                    }
                 }
             }
-        }
-        if (empty)
-            tile.setProperty("empty", true)
+            if (empty)
+                tile.setProperty("empty", true)
+            else
+                tile.removeProperty("empty")
+        })
+        if (numEmpty > 0)
+            tileset.setProperty("numempty", numEmpty)
         else
-            tile.removeProperty("empty")
+            tileset.removeProperty("numempty")
     })
-    tileset.setProperty("numempty", numEmpty)
+}
+
+/**
+ * 
+ * @param {TileLayer} layer 
+ */
+function TileLayer_clearEmptyTiles(layer) {
+    if (!layer.isTileLayer) return
+
+    let editor = layer.edit()
+    layer.region().rects.forEach(rect => {
+        let x2 = x + rect.width, y2 = y + rect.height
+        for (let y = rect.y; y < y2; ++y)
+            for (let x = rect.x; x < x2; ++x)
+                if (layer.tileAt(x, y).property("empty") === true)
+                    editor.setTile(x, y, null)
+        
+    })
+    editor.apply()
+}
+
+/**
+ * 
+ * @param {TileMap} map 
+ */
+function TileMap_updateEmptyTilesetTiles(map) {
+    map.macro("Update empty tileset tiles", () => {
+        map.tilesets.forEach(Tileset_updateEmptyTiles)
+    })
+}
+
+/**
+ * 
+ * @param {TileMap} map 
+ */
+function TileMap_clearEmptyLayerTiles(map) {
+    map.macro("Clear empty layer tiles", () => {
+        map.layers.forEach(TileLayer_clearEmptyTiles)
+    })
 }
 
 tiled.registerAction('MarkAndCountEmptyTiles',
     action => {
         let asset = tiled.activeAsset
         if (asset.isTileset)
-            markAndCountEmptyTiles(asset)
+            Tileset_updateEmptyTiles(asset)
         else if (asset.isTileMap) {
-            /** @type {TileMap} */
-            let map = asset
-            map.tilesets.map(tileset =>
-                markAndCountEmptyTiles(tileset)
-            )
+            TileMap_updateEmptyTilesetTiles(asset)
+            TileMap_clearEmptyLayerTiles(asset)
         }
     }).text = 'Mark and count empty tiles';
 
@@ -51,8 +92,16 @@ tiled.extendMenu("Tileset", [
 ]);
 
 tiled.assetAboutToBeSaved.connect(asset => {
+    if (asset.resolvedProperty("OnSaveInspectForEmptyTiles") !== true)
+        return
+
     if (asset.isTileset) {
-        if (asset.resolvedProperty("OnSaveInspectForEmptyTiles") === true)
-            markAndCountEmptyTiles(asset)
+        Tileset_updateEmptyTiles(asset)
+    } else if (asset.isTileMap) {
+        /** @type {TileMap} */
+        let map = asset
+        TileMap_updateEmptyTilesetTiles(map)
+        TileMap_clearEmptyLayerTiles(map)
+        map.tilesets.forEach(tileset => tileset.save())
     }
 })
