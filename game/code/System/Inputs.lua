63e4c936b95939ee82161822a14c5b324fd1d0e3
IoriBranford <ioribranford@gmail.com>
2025-05-05 21:29:30 -0700

    Alternative, more concise input mapping method

--- game/code/System/Inputs.lua
+++ game/code/System/Inputs.lua
@@ -148,6 +148,109 @@ function Inputs.addMapping(inputstring, actionname)
     end
 end
 
+function Inputs.addKeyMapping(keys, actionname)
+    local key1, key2 = string.match(keys, "^(%S+) *(%S*)$")
+    local input
+
+    if (key2 or "") ~= "" then
+        ---@type KeyAxisInput
+        input = {
+            type = "keyaxis",
+            negative = key1,
+            positive = key2,
+        }
+        print(key1, key2)
+    elseif key1 then
+        ---@type KeyInput
+        input = {
+            type = "key",
+            key = key1,
+        }
+    end
+
+    if input then
+        local action = Inputs.getOrMakeAction(actionname)
+        inputs[input] = action
+        return action
+    end
+end
+
+function Inputs.addKeyboardMappings(mappings)
+    for keys, actionname in pairs(mappings) do
+        Inputs.addKeyMapping(keys, actionname)
+    end
+end
+
+local GamepadInputTypes = {
+    leftx = "axis",
+    lefty = "axis",
+    rightx = "axis",
+    righty = "axis",
+    triggerleft = "axis",
+    triggerright = "axis",
+    a = "button",
+    b = "button",
+    x = "button",
+    y = "button",
+    leftstick = "button",
+    rightstick = "button",
+    leftshoulder = "button",
+    rightshoulder = "button",
+    back = "button",
+    start = "button",
+    dpup = "button",
+    dpdown = "button",
+    dpleft = "button",
+    dpright = "button",
+    guide = "button",
+    touchpad = "button",
+    paddle1 = "button",
+    paddle2 = "button",
+    paddle3 = "button",
+    paddle4 = "button",
+    misc1 = "button"
+}
+
+function Inputs.addGamepadInputMapping(gamepadid, gamepadinputs, actionname)
+    local input1, input2 = string.match(gamepadinputs, "^(%S+) *(%S*)$")
+    local input
+    if input2 then
+        input = {
+            type = "gamepadbuttonaxis",
+            gamepadid = gamepadid,
+            negative = input1,
+            positive = input2,
+        }
+    elseif input1 then
+        local inputtype = GamepadInputTypes[input1]
+        if inputtype then
+            input = {
+                type = "gamepad"..inputtype,
+                gamepadid = gamepadid,
+                [inputtype] = input1
+            }
+        end
+    end
+
+    if input then
+        local action = Inputs.getOrMakeAction(actionname)
+        inputs[input] = action
+        return action
+    end
+end
+
+function Inputs.addGamepadMappings(gamepadid, mappings)
+    for gamepadinputs, actionname in pairs(mappings) do
+        Inputs.addGamepadInputMapping(gamepadid, gamepadinputs, actionname)
+    end
+end
+
+function Inputs.addGamepadsMappings(mappings)
+    for gamepadid, gamepadmappings in ipairs(mappings) do
+        Inputs.addGamepadMappings(gamepadid, gamepadmappings)
+    end
+end
+
 function Inputs.addMappings(mappings)
     for inputstring, actionname in pairs(mappings) do
         Inputs.addMapping(inputstring, actionname)
