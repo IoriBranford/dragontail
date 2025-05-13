2bc928be8d5803ad18ef91122268b8801fe84977
IoriBranford <ioribranford@gmail.com>
2025-05-13 15:31:20 -0700

    Dish shatter sprite frames

--- game/data/sprites/weapons/Dishes A.tsx
+++ game/data/sprites/weapons/Dishes A.tsx
@@ -1,9 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.0" name="Dishes A" tilewidth="32" tileheight="32" tilecount="16" columns="8" objectalignment="center">
- <image source="../banditcave/Dishes A.png" width="256" height="64"/>
+<tileset version="1.11" tiledversion="1.11.2" name="Dishes A" tilewidth="32" tileheight="32" tilecount="24" columns="8" objectalignment="center">
+ <image source="../banditcave/Dishes A.png" width="256" height="96"/>
  <tile id="0" type="item-dish">
   <properties>
    <property name="name" value="dish-small"/>
   </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="Shatter"/>
+  </properties>
+  <animation>
+   <frame tileid="16" duration="50"/>
+   <frame tileid="17" duration="50"/>
+   <frame tileid="18" duration="50"/>
+   <frame tileid="19" duration="50"/>
+  </animation>
  </tile>
 </tileset>
