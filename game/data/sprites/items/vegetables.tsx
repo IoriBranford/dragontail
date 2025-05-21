1b6b6a2ee70b99cd74fedfcf0c5d55a14d91e79a
IoriBranford <ioribranford@gmail.com>
2025-05-21 15:24:33 -0700

    daneeklu vegetables for fire pepper

---
+++ game/data/sprites/items/vegetables.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="vegetables" tilewidth="32" tileheight="32" tilecount="7" columns="7" objectalignment="bottom">
+ <tileoffset x="0" y="8"/>
+ <image source="vegetables.png" width="224" height="32"/>
+ <tile id="0" type="mana-small"/>
+ <tile id="1" type="food-small"/>
+ <tile id="2" type="food-small"/>
+ <tile id="3" type="food-small"/>
+ <tile id="4" type="food-small"/>
+ <tile id="5" type="food-small"/>
+</tileset>
