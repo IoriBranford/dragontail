e1df3c931d8707c5a30302c1c909bdd3be598193
IoriBranford <ioribranford@gmail.com>
2025-05-22 19:21:02 -0700

    Data for peppers item that can be dropped from enemies and containers

--- game/data/sprites/items/vegetables.tsx
+++ game/data/sprites/items/vegetables.tsx
@@ -2,7 +2,11 @@
 <tileset version="1.11" tiledversion="1.11.2" name="vegetables" tilewidth="32" tileheight="32" tilecount="7" columns="7" objectalignment="bottom">
  <tileoffset x="0" y="8"/>
  <image source="vegetables.png" width="224" height="32"/>
- <tile id="0" type="mana-small"/>
+ <tile id="0" type="mana-small">
+  <properties>
+   <property name="name" value="peppers"/>
+  </properties>
+ </tile>
  <tile id="1" type="food-small"/>
  <tile id="2" type="food-small"/>
  <tile id="3" type="food-small"/>
