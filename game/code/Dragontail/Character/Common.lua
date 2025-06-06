e4539409d2ecb78f0d423c4551c8262a0c869c2f
IoriBranford <ioribranford@gmail.com>
2025-06-06 11:48:00 -0700

    Make breakable door pulse red

--- game/code/Dragontail/Character/Common.lua
+++ game/code/Dragontail/Character/Common.lua
@@ -447,11 +447,11 @@ function Common:guardHit(attacker)
     return self.recoverai or self.initialai
 end
 
-function Common:pulseColor()
+function Common:pulseRed()
     local t = 0
     local r, g, b, a = 1, 1, 1, 1
     while true do
-        r = (1 + math.cos(t)) / 2
+        g = (1 + math.cos(t)) / 2
         b = r
         self.color = Color.asARGBInt(r, g, b, a)
         yield()
