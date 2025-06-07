9917b8a7dddf737b721490c505e801fe77c220ec
IoriBranford <ioribranford@gmail.com>
2025-06-07 16:18:13 -0700

    Update copyright and project name

--- make-vars.sh
+++ make-vars.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export PROJECT=dragontail
+export PROJECT=rose-of-dragontail
 export GAME_TITLE="Rose of Dragontail"
 export GAME_TITLE_NOSPACE=Rose_of_Dragontail
 export DESCRIPTION="Dragon-girl beat-em-up game"
-export COPYRIGHT="© 2022-2024 IoriBranford"
+export COPYRIGHT="© 2022-2025 IoriBranford"
 export LOVE_VERSION="11.5"
 export GAME_DIR="$PWD/game"
