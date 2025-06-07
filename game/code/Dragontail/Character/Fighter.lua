6292563e4d12731f1b2371768b09bbad6d457db9
IoriBranford <ioribranford@gmail.com>
2025-06-07 01:36:45 -0700

    Restore extra yield in wallSlammed so forge-fire can interrupt wallSlammed state

--- game/code/Dragontail/Character/Fighter.lua
+++ game/code/Dragontail/Character/Fighter.lua
@@ -364,6 +364,7 @@ function Fighter:wallSlammed(thrower, oobx, ooby)
     self.health = self.health - (self.wallslamdamage or 25)
     self.velx, self.vely, self.velz = 0, 0, 0
     self:stopAttack()
+    yield() -- a window to be juggled by damaging wall e.g. forge-fire
     self.hurtstun = self.wallslamstun or 20
     yield()
     local nextstate, a, b, c, d, e, f = self:afterWallSlammed(thrower)
