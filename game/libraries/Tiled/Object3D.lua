7942bbfd7c01c978adaedd422351304639b8ae41
IoriBranford <ioribranford@gmail.com>
2025-07-15 18:45:46 -0700

    3D aseprite object init and animate

--- game/libraries/Tiled/Object3D.lua
+++ game/libraries/Tiled/Object3D.lua
@@ -27,4 +27,21 @@ function Object3D:animateTile(dt)
     end
 end
 
+function Object3D:initAseprite()
+    local aseprite = self.aseprite
+    if not aseprite then return end
+    self.model = self.aseprite:newModel()
+    self.animate = Object3D.animateAseprite
+    self.draw = drawModel
+end
+
+---@param self Object3D
+function Object3D:animateAseprite(dt)
+    local oldframei = self.animationframe
+    self:animateAseprite(dt)
+    if oldframei ~= self.animationframe then
+        self.aseprite:updateModel(self.model, self.animationframe)
+    end
+end
+
 return Object3D
