175b52a2673355ce1c6db17dbbbf6ddd7237c16c
IoriBranford <ioribranford@gmail.com>
2025-07-15 17:53:50 -0700

    Common drawModel function in its own file

---
+++ game/libraries/Tiled/drawModel.lua
@@ -0,0 +1,23 @@
+---@param self Layer|Object3D
+---@param fixedfrac number
+local function drawModel(self, fixedfrac)
+    local model = self.model
+    if not model then return end
+
+    local x, y, z = self.x, self.y, self.z
+    x = x + (self.velx or 0)*fixedfrac
+    y = y + (self.vely or 0)*fixedfrac
+    z = z + (self.velz or 0)*fixedfrac
+    model:setTranslation(x, -y, z)
+    local rotaxisx = self.rotaxisx
+    local rotaxisy = self.rotaxisy
+    local rotaxisz = self.rotaxisz
+    if not (rotaxisx and rotaxisy and rotaxisz) then
+        rotaxisx, rotaxisy, rotaxisz = 0, 0, -1
+    end
+    model:setAxisAngleRotation(rotaxisx, rotaxisy, rotaxisz, self.rotation)
+    model:setScale(self.scalex or 1, self.scaley or 1, self.scalez or 1)
+    model:draw()
+end
+
+return drawModel
