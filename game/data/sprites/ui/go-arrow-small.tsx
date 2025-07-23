917832a1bd2244431c57a8ded6aa4183aca10966
IoriBranford <ioribranford@gmail.com>
2025-07-22 18:26:05 -0700

    Smaller "GO" arrow asset

---
+++ game/data/sprites/ui/go-arrow-small.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="go-arrow-small" tilewidth="80" tileheight="10" tilecount="8" columns="1" objectalignment="center">
+ <image source="go-arrow-small.png" width="80" height="80"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
