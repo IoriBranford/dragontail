ed37e8e8c4e96bf9fa62e2d7d834d0dd27d5d282
IoriBranford <ioribranford@gmail.com>
2025-07-22 18:26:44 -0700

    "GO" word and arrow as tilesets

---
+++ game/data/sprites/ui/go-arrow.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="go-arrow" tilewidth="128" tileheight="16" tilecount="8" columns="1" objectalignment="center">
+ <image source="go-arrow.png" width="128" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ game/data/sprites/ui/go-word.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="go-word" tilewidth="40" tileheight="20" tilecount="6" columns="6" objectalignment="center">
+ <image source="go-word.png" width="240" height="20"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
