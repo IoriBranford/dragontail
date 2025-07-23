56f4574a4789a0307965ed2224120a3e93ef8458
IoriBranford <ioribranford@gmail.com>
2025-07-23 15:59:39 -0700

    Add size pulse to "GO" word and arrow

--- game/data/sprites/ui/go-arrow-small.tsx
+++ game/data/sprites/ui/go-arrow-small.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="go-arrow-small" tilewidth="80" tileheight="10" tilecount="8" columns="1" objectalignment="center">
- <image source="go-arrow-small.png" width="80" height="80"/>
+<tileset version="1.11" tiledversion="1.11.2" name="go-arrow-small" tilewidth="100" tileheight="20" tilecount="9" columns="1" objectalignment="center">
+ <image source="go-arrow-small.png" width="100" height="180"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="50"/>
@@ -11,6 +11,7 @@
    <frame tileid="5" duration="50"/>
    <frame tileid="6" duration="50"/>
    <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="100"/>
   </animation>
  </tile>
 </tileset>
--- game/data/sprites/ui/go-word.tsx
+++ game/data/sprites/ui/go-word.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="go-word" tilewidth="40" tileheight="20" tilecount="6" columns="6" objectalignment="center">
- <image source="go-word.png" width="240" height="20"/>
+<tileset version="1.11" tiledversion="1.11.2" name="go-word" tilewidth="48" tileheight="32" tilecount="9" columns="9" objectalignment="center">
+ <image source="go-word.png" width="432" height="32"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="50"/>
@@ -9,6 +9,9 @@
    <frame tileid="3" duration="50"/>
    <frame tileid="4" duration="50"/>
    <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="100"/>
   </animation>
  </tile>
 </tileset>
