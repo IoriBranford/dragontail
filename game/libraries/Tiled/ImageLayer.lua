080b3b52e3aca662d3b8240093aebb9e834e1f79
IoriBranford <ioribranford@gmail.com>
2025-07-23 18:52:54 -0700

    ImageLayer make3D and rotationdegrees

--- game/libraries/Tiled/ImageLayer.lua
+++ game/libraries/Tiled/ImageLayer.lua
@@ -3,6 +3,7 @@ local Assets = require "Tiled.Assets"
 local Color  = require "Tiled.Color"
 local Layer  = require "Tiled.Layer"
 local pathlite = require "Tiled.pathlite"
+local drawModel= require "Tiled.drawModel"
 
 ---@class ImageLayer:Layer
 ---@field type string "imagelayer"
@@ -11,6 +12,7 @@ local pathlite = require "Tiled.pathlite"
 ---@field imagefile string
 ---@field image love.Image|string
 ---@field shader love.Shader?
+---@field rotationdegrees number?
 local ImageLayer = class(Layer)
 
 function ImageLayer:_init(directory)
@@ -20,6 +22,9 @@ function ImageLayer:_init(directory)
     end
     self.imagefile = imagefile
     self.image = Assets.get(self.imagefile)
+    if self.rotationdegrees then
+        self.rotation = math.rad(self.rotationdegrees)
+    end
 end
 
 function ImageLayer:draw()
@@ -31,6 +36,36 @@ function ImageLayer:draw()
         self.scalex or 1, self.scaley or 1,
         self.originx or 0, self.originy or 0,
         self.skewx or 0, self.skewy or 0)
+end
+
+function ImageLayer:make3D()
+    local g3d = require "g3d" ---@type g3d
+    local image = self.image
+    local w, h = image:getDimensions()
+    local x1 = -(self.originx or 0)
+    local y1 = -(self.originy or 0)
+    local x2 = x1 + w
+    local y2 = y1 + h
+    local u0, v0 = 0, 0
+    local u1, v1 = 1, 1
+    local repeats = 128
+    if self.repeatx then
+        u1 = repeats*2
+        x1 = x1 - repeats*w
+        x2 = x2 + (repeats-1)*w
+    end
+    if self.repeaty then
+        v1 = repeats*2
+        y1 = y1 - repeats*h
+        y2 = y2 + (repeats-1)*h
+    end
+    local tl = { x1,-y1,0, u0,v0, 0,0,1, 1,1,1,1 }
+    local bl = { x1,-y2,0, u0,v1, 0,0,1, 1,1,1,1 }
+    local tr = { x2,-y1,0, u1,v0, 0,0,1, 1,1,1,1 }
+    local br = { x2,-y2,0, u1,v1, 0,0,1, 1,1,1,1 }
+    local verts = {tl, tr, bl, tr, bl, br}
+    self.model = g3d.newModel(verts, self.image)
+    self.draw = drawModel
 end
 
 return ImageLayer
