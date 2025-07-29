8a4f265b7fa4043cd204572cfa95b2124b971ac8
IoriBranford <ioribranford@gmail.com>
2025-07-28 22:17:42 -0700

    Update size and offset for Rose aseprite tileset

--- game/data/sprites/rose-new.tsx
+++ game/data/sprites/rose-new.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="rose-new" tilewidth="64" tileheight="64" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="8"/>
- <image source="rose-new.ase" width="64" height="64"/>
+<tileset version="1.11" tiledversion="1.11.2" name="rose-new" tilewidth="80" tileheight="80" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="24"/>
+ <image source="rose-new.ase" width="80" height="80"/>
 </tileset>
