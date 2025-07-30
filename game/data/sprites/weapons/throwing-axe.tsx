dc98d812c9f0ceff7485ba20caa15d9db64ff949
IoriBranford <ioribranford@gmail.com>
2025-07-30 00:17:33 -0700

    Axe tileset and template

---
+++ game/data/sprites/weapons/throwing-axe.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="throwing-axe" tilewidth="48" tileheight="48" tilecount="1" columns="1" objectalignment="center">
+ <image source="throwing-axe.ase" width="48" height="48"/>
+</tileset>
