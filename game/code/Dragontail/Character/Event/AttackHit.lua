920406ec18d3d7f9401d1690b52e82eb89a87dab
IoriBranford <ioribranford@gmail.com>
2025-08-13 20:48:02 -0700

    AttackHit constructor

--- game/code/Dragontail/Character/Event/AttackHit.lua
+++ game/code/Dragontail/Character/Event/AttackHit.lua
@@ -1,3 +1,6 @@
+local tablepool = require "tablepool"
+local Guard     = require "Dragontail.Character.Action.Guard"
+
 ---@class AttackHit
 ---@field attacker Attacker
 ---@field attack Attack
@@ -12,3 +15,23 @@
 ---@field peney number?
 ---@field penez number?
 ---@field guarded boolean
+
+return function(attacker, target, penex, peney, penez)
+    local Attacker  = require "Dragontail.Character.Component.Attacker"
+    local x, y, z, r, h = Attacker.getAttackCylinder(attacker)
+    local hit = tablepool.fetch("AttackHit", 0, 16)
+    hit.angle = attacker.attackangle
+    hit.attack = attacker.attack
+    hit.target = target
+    hit.attacker = attacker
+    hit.penex = penex
+    hit.peney = peney
+    hit.penez = penez
+    hit.attackx = x
+    hit.attacky = y
+    hit.attackz = z
+    hit.attackr = r
+    hit.attackh = h
+    hit.guarded = Guard.isAttackInGuardArc(target, attacker)
+    return hit
+end
