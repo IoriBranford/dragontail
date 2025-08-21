2880d65d3d6e4a9d38fac4d2f3a0b87ae1828ea3
IoriBranford <ioribranford@gmail.com>
2025-08-21 16:12:49 -0700

    Display projectile trajectory while preparing to shoot

--- game/code/Dragontail/Character/Behavior/Enemy/AttackPrepare.lua
+++ game/code/Dragontail/Character/Behavior/Enemy/AttackPrepare.lua
@@ -1,13 +1,18 @@
 local Behavior = require "Dragontail.Character.Behavior"
 local Guard    = require "Dragontail.Character.Action.Guard"
 local Color    = require "Tiled.Color"
+local Shoot    = require "Dragontail.Character.Action.Shoot"
+local Characters = require "Dragontail.Stage.Characters"
 
+---@class AttackPrepare:Behavior
 local AttackPrepare = pooledclass(Behavior)
+AttackPrepare._nrec = Behavior._nrec + 1
 
 function AttackPrepare:start()
     local enemy = self.character
     enemy.numopponentshit = 0
     Guard.stopGuarding(enemy)
+    self.trajectory = enemy.attack.projectiletype and {}
 end
 
 function AttackPrepare:fixedupdate()
@@ -15,6 +20,24 @@ function AttackPrepare:fixedupdate()
     enemy.color = enemy:getAttackFlashColor(enemy.statetime or 0, enemy.canbeattacked)
 
     local target = enemy.opponents[1]
+    local trajectory = self.trajectory
+    if trajectory then
+        Shoot.calculateTrajectoryTowardsTarget(enemy, enemy.attack.projectiletype,
+            target.x, target.y, target.z, trajectory)
+        for i = 3, #trajectory, 3 do
+            local point = {
+                x = trajectory[i-2],
+                y = trajectory[i-1],
+                z = trajectory[i],
+                type = "projectile-path-point"
+            }
+            Characters.spawn(point)
+        end
+        for i = #trajectory, 1, -1 do
+            trajectory[i] = nil
+        end
+    end
+
     local state, a, b, c, d, e, f = enemy:duringPrepareAttack(target)
     if state then
         return state, a, b, c, d, e, f
