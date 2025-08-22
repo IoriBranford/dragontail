537b6ac6093c65f8423d05c68c02b657849a4b2c
IoriBranford <ioribranford@gmail.com>
2025-08-21 18:10:32 -0700

    Set trajectory scale and alpha to indicate time to fire

--- game/code/Dragontail/Character/Behavior/Enemy/AttackPrepare.lua
+++ game/code/Dragontail/Character/Behavior/Enemy/AttackPrepare.lua
@@ -5,6 +5,7 @@ local Shoot    = require "Dragontail.Character.Action.Shoot"
 local Characters = require "Dragontail.Stage.Characters"
 
 ---@class AttackPrepare:Behavior
+---@field character Enemy
 local AttackPrepare = pooledclass(Behavior)
 AttackPrepare._nrec = Behavior._nrec + 1
 
@@ -24,14 +25,26 @@ function AttackPrepare:fixedupdate()
     if trajectory then
         Shoot.calculateTrajectoryTowardsTarget(enemy, enemy.attack.projectiletype,
             target.x, target.y, target.z, trajectory)
+
+        local statetime = enemy.state.statetime or 1
+        local timeleft = enemy.statetime or 0
+        local scale = 1 + timeleft/statetime
+        local alpha = math.max(0, math.min(1, 1 - timeleft/statetime))
         for i = 3, #trajectory, 3 do
-            local point = {
-                x = trajectory[i-2],
-                y = trajectory[i-1],
-                z = trajectory[i],
-                type = "projectile-path-point"
+            local x = trajectory[i-2]
+            local y = trajectory[i-1]
+            local z = trajectory[i]
+            local point = Characters.spawn {
+                x = x,
+                y = y,
+                z = z,
+                type = "projectile-path-point",
+                scalex = scale,
+                scaley = scale,
             }
-            Characters.spawn(point)
+            local color = point.color or Color.Red
+            local r,g,b = Color.unpack(color)
+            point.color = Color.asARGBInt(r, g, b, alpha)
         end
         for i = #trajectory, 1, -1 do
             trajectory[i] = nil
