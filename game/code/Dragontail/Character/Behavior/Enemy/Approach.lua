00644ee962860c9d28a8709e8a18869604178314
IoriBranford <ioribranford@gmail.com>
2025-08-29 18:38:00 -0700

    A behavior version of Approach that updates the destination point as player moves

---
+++ game/code/Dragontail/Character/Behavior/Enemy/Approach.lua
@@ -0,0 +1,61 @@
+local Behavior = require "Dragontail.Character.Behavior"
+local Face     = require "Dragontail.Character.Component.Face"
+local Movement = require "Component.Movement"
+
+local Approach = pooledclass(Behavior)
+Approach._nrec = Behavior._nrec + 3
+
+function Approach:start(nextattacktype)
+    local enemy = self.character
+    local opponent = enemy.opponents[1] ---@type Player
+    self.nextattacktype = nextattacktype
+    self.attackerslot = enemy:findAttackerSlot(opponent, nextattacktype)
+    self.reached = false
+end
+
+function Approach:fixedupdate()
+    local enemy = self.character
+    local opponent = enemy.opponents[1] ---@type Player
+
+    local attackerslot = self.attackerslot
+    local destx, desty = enemy.x, enemy.y
+    if attackerslot then
+        destx, desty = enemy:getAttackerSlotPosition(attackerslot, self.nextattacktype)
+    end
+
+    local speed = enemy.speed or 2
+    self.reached = math.distsq(enemy.x, enemy.y, destx, desty) < speed
+    if self.reached then
+        return self:timeout()
+    end
+
+    if math.distsq(enemy.x, enemy.y, opponent.x, opponent.y) > 320*320 then
+        speed = speed * 1.5
+    end
+
+    destx, desty = enemy:navigateAroundSolid(destx, desty)
+    Face.faceVector(enemy, destx - enemy.x, desty - enemy.y, "Walk")
+    local state, a, b, c, d, e, f = enemy:duringApproach(opponent)
+    if state then
+        return state, a, b, c, d, e, f
+    end
+    enemy.velx, enemy.vely = Movement.getVelocity_speed(enemy.x, enemy.y, destx, desty, speed)
+end
+
+function Approach:timeout()
+    local enemy = self.character
+    local opponent = enemy.opponents[1] ---@type Player
+    if enemy:couldAttackOpponent(opponent, self.nextattacktype) then
+        opponent.attacker = enemy
+        Face.facePosition(enemy, opponent.x, opponent.y)
+        return self.nextattacktype
+    end
+    -- enemy:debugPrint_couldAttackOpponent(opponent, nextattacktype)
+
+    if self.reached then
+        return "stand", 10
+    end
+    return "stand", 0
+end
+
+return Approach
