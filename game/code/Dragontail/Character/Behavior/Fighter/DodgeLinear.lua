6ff008a2a021c6de4ff912d1b5b362e6def2149e
IoriBranford <ioribranford@gmail.com>
2025-09-04 17:24:32 -0700

    Linear dodge behavior class

---
+++ game/code/Dragontail/Character/Behavior/Fighter/DodgeLinear.lua
@@ -0,0 +1,40 @@
+local Face = require "Dragontail.Character.Component.Face"
+local Slide= require "Dragontail.Character.Action.Slide"
+local Body = require "Dragontail.Character.Component.Body"
+local Behavior = require "Dragontail.Character.Behavior"
+
+local DodgeLinear = pooledclass(Behavior)
+DodgeLinear._nrec = Behavior._nrec + 3
+
+function DodgeLinear:start(dodgeangle)
+    local fighter = self.character
+    local opponent = fighter.opponents[1]
+    local x, y, oppox, oppoy = fighter.x, fighter.y, opponent.x, opponent.y
+    local tooppox, tooppoy = oppox - x, oppoy - y
+    if tooppox == 0 and tooppoy == 0 then
+        tooppox = 1
+    end
+    tooppox, tooppoy = math.norm(tooppox, tooppoy)
+    Face.faceVector(fighter, tooppox, tooppoy)
+    self.opponent = opponent
+    self.dodgeangle = dodgeangle
+    self.speed = fighter.dodgespeed or 1
+    self.speed = Slide.updateSlideSpeed(fighter, self.dodgeangle, self.speed, 1/(fighter.mass or 1))
+end
+
+function DodgeLinear:fixedupdate()
+    local fighter = self.character
+    local newstate, a, b, c, d, e, f = fighter:duringDodge(self.opponent)
+    if newstate then
+        return newstate, a, b, c, d, e, f
+    end
+    Body.keepInBounds(fighter)
+    self.speed = Slide.updateSlideSpeed(fighter, self.dodgeangle, self.speed, 1/(fighter.mass or 1))
+    if not fighter.statetime then
+        if self.speed == 0 then
+            return "stand"
+        end
+    end
+end
+
+return DodgeLinear
