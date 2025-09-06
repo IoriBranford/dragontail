f6fb6dbccac15b9d4ed5a6d13d0eef0d1b3c0ee2
IoriBranford <ioribranford@gmail.com>
2025-09-05 17:09:47 -0700

    Add test enemy sprite asset

---
+++ game/data/sprites/test/enemy.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="enemy" tilewidth="128" tileheight="128" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="40"/>
+ <image source="enemy.ase" width="128" height="128"/>
+</tileset>
