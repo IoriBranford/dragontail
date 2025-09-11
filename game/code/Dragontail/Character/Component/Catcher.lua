0dc2804ebb7e739884479aa0efcc34ae5199cfde
IoriBranford <ioribranford@gmail.com>
2025-09-11 13:37:26 -0700

    Catcher component

---
+++ game/code/Dragontail/Character/Component/Catcher.lua
@@ -0,0 +1,37 @@
+---@class Catcher:Body
+---@field catchradius number
+---@field catcharc number
+local Catcher = {}
+
+function Catcher:findCharacterToCatch(characters, catchdirx, catchdiry)
+    local catchradius = (self.catchradius or 20)
+    local coscatcharc = math.cos(self.catcharc or (math.pi/4))
+    local x, y, z = self.x, self.y, self.z
+    local ztop = z + self.bodyheight + catchradius/2
+    for _, character in ipairs(characters) do
+        if self ~= character.thrower
+        and character:isAttacking()
+        and character.z >= z
+        and character.z <= ztop
+        then
+            local catchprojradius = catchradius
+                + math.max(character.attack.radius,
+                            character.bodyradius)
+            local mindot = coscatcharc * catchprojradius
+            local toprojx = character.x - x
+            local toprojy = character.y - y
+            local d = math.dot(catchdirx, catchdiry, toprojx, toprojy)
+            if mindot <= d and d <= catchprojradius then
+                return character
+            end
+            local toprojx2 = toprojx + character.velx
+            local toprojy2 = toprojy + character.vely
+            d = math.dot(catchdirx, catchdiry, toprojx2, toprojy2)
+            if mindot <= d and d <= catchprojradius then
+                return character
+            end
+        end
+    end
+end
+
+return Catcher
