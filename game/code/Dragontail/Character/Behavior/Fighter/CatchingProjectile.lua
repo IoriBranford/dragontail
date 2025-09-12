e3118b80dcb16fba7298fee23a87ac431b77eb69
IoriBranford <ioribranford@gmail.com>
2025-09-11 17:25:21 -0700

    Move CatchingProjectile behavior to Fighter

--- game/code/Dragontail/Character/Behavior/Fighter/CatchingProjectile.lua
+++ game/code/Dragontail/Character/Behavior/Fighter/CatchingProjectile.lua
@@ -1,24 +1,25 @@
 local Behavior = require "Dragontail.Character.Behavior"
 local Face     = require "Dragontail.Character.Component.Face"
 local StateMachine = require "Dragontail.Character.Component.StateMachine"
-local Body         = require "Dragontail.Character.Component.Body"
 
+---@class CatchingProjectile:Behavior
+---@field character Fighter
 local CatchingProjectile = pooledclass(Behavior)
 
 function CatchingProjectile:start(projectile)
-    local player = self.character
-    Face.faceObject(player, projectile, player.state.animation or "catch")
+    local fighter = self.character
+    Face.faceObject(fighter, projectile, fighter.state.animation or "catch")
     projectile:stopAttack()
-    if player:tryToGiveWeapon(projectile.type) then
+    if fighter:tryToGiveWeapon(projectile.type) then
         projectile:disappear()
     else
-        StateMachine.start(projectile, "projectileBounce", player)
+        StateMachine.start(projectile, "projectileBounce", fighter)
     end
 end
 
 function CatchingProjectile:fixedupdate()
-    local player = self.character
-    Body.accelerateTowardsVel(player, 0, 0, player.mass or 1)
+    local fighter = self.character
+    fighter:decelerateXYto0()
 end
 
 return CatchingProjectile
