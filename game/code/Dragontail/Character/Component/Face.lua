cb3e134f064b2e600efb80a7716fdba1b46c9338
IoriBranford <ioribranford@gmail.com>
2025-09-17 14:45:44 -0700

    Face functions return the face angle

--- game/code/Dragontail/Character/Component/Face.lua
+++ game/code/Dragontail/Character/Component/Face.lua
@@ -18,39 +18,44 @@ function Face:faceObject(object, animation, frame1, loopframe)
     if object then
         Face.facePosition(self, object.x, object.y, animation, frame1, loopframe)
     end
+    return self.faceangle
 end
 
 function Face:facePosition(x, y, animation, frame1, loopframe)
-    Face.faceVector(self, x - self.x, y - self.y, animation, frame1, loopframe)
+    return Face.faceVector(self, x - self.x, y - self.y, animation, frame1, loopframe)
 end
 
 function Face:faceVector(vx, vy, animation, frame1, loopframe)
-    Face.faceAngle(self, (vx ~= 0 or vy ~= 0) and math.atan2(vy, vx), animation, frame1, loopframe)
+    return Face.faceAngle(self, (vx ~= 0 or vy ~= 0) and math.atan2(vy, vx), animation, frame1, loopframe)
 end
 
 function Face:faceAngle(angle, animation, frame1, loopframe)
     if angle then
         self.faceangle = angle
+    else
+        angle = self.faceangle
     end
     if animation then
-        DirectionalAnimation.set(self, animation, angle or self.faceangle, frame1, loopframe)
+        DirectionalAnimation.set(self, animation, angle, frame1, loopframe)
     end
+    return angle
 end
 
 function Face:turnTowardsAngle(destangle, turnspeed, animation, frame1, loopframe)
     local faceangle = math.rotangletowards(self.faceangle, destangle,
         turnspeed or self.faceturnspeed or math.pi)
-    Face.faceAngle(self, faceangle, animation, frame1, loopframe)
+    return Face.faceAngle(self, faceangle, animation, frame1, loopframe)
 end
 
 function Face:turnTowardsVector(vecx, vecy, turnspeed, animation, frame1, loopframe)
     if vecx ~= 0 or vecy ~= 0 then
         Face.turnTowardsAngle(self, math.atan2(vecy, vecx), turnspeed, animation, frame1, loopframe)
     end
+    return self.faceangle
 end
 
 function Face:turnTowardsPosition(x, y, turnspeed, animation, frame1, loopframe)
-    Face.turnTowardsVector(self, x - self.x, y - self.y, turnspeed, animation, frame1, loopframe)
+    return Face.turnTowardsVector(self, x - self.x, y - self.y, turnspeed, animation, frame1, loopframe)
 end
 
 ---@param object { x: number, y: number }
@@ -58,12 +63,14 @@ function Face:turnTowardsObject(object, turnspeed, animation, frame1, loopframe)
     if object then
         Face.turnTowardsPosition(self, object.x, object.y, turnspeed, animation, frame1, loopframe)
     end
+    return self.faceangle
 end
 
 function Face:updateTurnToDestAngle(turnspeed, animation, frame1, loopframe)
     if self.facedestangle then
         Face.turnTowardsAngle(self, self.facedestangle, turnspeed, animation, frame1, loopframe)
     end
+    return self.faceangle
 end
 
 return Face
