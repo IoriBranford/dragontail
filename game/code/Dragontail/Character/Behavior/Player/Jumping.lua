1d0207b6e17cd5e422f42caea092fe588d73d712
IoriBranford <ioribranford@gmail.com>
2025-09-17 14:30:35 -0700

    Turn towards joystick during jump to hint that jump attack can be directed

--- game/code/Dragontail/Character/Behavior/Player/Jumping.lua
+++ game/code/Dragontail/Character/Behavior/Player/Jumping.lua
@@ -1,6 +1,9 @@
 local Behavior = require "Dragontail.Character.Behavior"
 local Face     = require "Dragontail.Character.Component.Face"
 local Audio    = require "System.Audio"
+
+---@class PlayerJumping:Behavior
+---@field character Player
 local PlayerJumping = pooledclass(Behavior)
 
 function PlayerJumping:start(isjumpstart)
@@ -41,7 +44,7 @@ function PlayerJumping:fixedupdate()
     local animation = player.velz >= 1 and "JumpUp"
         or player.velz >= -1 and "JumpPeak"
         or "JumpDown"
-    Face.faceAngle(player, player.faceangle, animation, player.animationframe)
+    player:turnTowardsJoystick(animation, animation)
 end
 
 return PlayerJumping
