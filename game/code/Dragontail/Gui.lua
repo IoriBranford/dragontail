970acea5c270f9a163703eaa27339f664902b546
IoriBranford <ioribranford@gmail.com>
2025-09-18 18:27:52 -0700

    Set default actions module for menus too

--- game/code/Dragontail/Gui.lua
+++ game/code/Dragontail/Gui.lua
@@ -1,8 +1,10 @@
 local Tiled = require "Tiled"
 local Gui   = require "Gui"
 local GuiObject = require "Gui.GuiObject"
+local Menu      = require "Gui.Menu"
 
 GuiObject.actionsmodule = "Dragontail.GuiActions"
+Menu.actionsmodule = "Dragontail.GuiActions"
 
 local map = Tiled.Map.load("data/gui.lua")
 Tiled.Assets.markMapAssetsPermanent(map, true)
