fffc1de30d42cdd8c95bc159957eda5ad890f34e
IoriBranford <ioribranford@gmail.com>
2025-09-26 12:52:14 -0700

    Behaviors for enemy holding player

---
+++ game/code/Dragontail/Character/Behavior/Enemy/Hold.lua
@@ -0,0 +1,99 @@
+local Combo = require "Dragontail.Character.Component.Combo"
+local HoldOpponent = require "Dragontail.Character.Action.HoldOpponent"
+local Face         = require "Dragontail.Character.Component.Face"
+local StateMachine = require "Dragontail.Character.Component.StateMachine"
+local Behavior     = require "Dragontail.Character.Behavior"
+
+---@class EnemyHold:Behavior
+---@field character Enemy
+local EnemyHold = pooledclass(Behavior)
+EnemyHold._nrec = Behavior._nrec + 2
+
+function EnemyHold:start(player)
+    local enemy = self.character
+    local holddirx, holddiry = player.x - enemy.x, player.y - enemy.y
+    if holddirx == 0 and holddiry == 0 then
+        holddirx, holddiry = math.cos(enemy.faceangle), math.sin(enemy.faceangle)
+    else
+        holddirx, holddiry = math.norm(holddirx, holddiry)
+    end
+
+    enemy.holdangle = math.atan2(holddiry, holddirx)
+    self.holddestangle = enemy.holdangle
+    self.isfrombehind = math.dot(holddirx, holddiry,
+        math.cos(player.faceangle), math.sin(player.faceangle)) >= 0
+
+    enemy:stopAttack()
+    if enemy.heldopponent ~= player then
+        Combo.reset(enemy)
+        HoldOpponent.startHolding(enemy, player)
+    end
+end
+
+function EnemyHold:fixedupdate()
+    local enemy = self.character
+    local player = enemy.heldopponent
+    if not player then
+        return enemy.recoverai or enemy.initialai
+    end
+
+    local holdangle = enemy.holdangle
+
+    --- TODO each enemy's movement and turning while holding
+    local targetvelx, targetvely = 0, 0
+    local inx, iny = 0, 0
+    -- local speed = 2
+    -- if inx ~= 0 or iny ~= 0 then
+    --     inx, iny = math.norm(inx, iny)
+    --     self.holddestangle = math.atan2(iny, inx)
+    --     targetvelx = inx * speed
+    --     targetvely = iny * speed
+    -- end
+
+    --- TODO enemy's decision to attack
+    local normalattackpressed = false
+    if normalattackpressed and (inx ~= 0 or iny ~= 0) then
+        Combo.reset(enemy)
+        return "spinning-throw", holdangle, player
+    end
+    if normalattackpressed then
+        return enemy:doComboAttack(holdangle, player, inx ~= 0 or iny ~= 0)
+    end
+
+    --- TODO enemy's decision to run with player
+    local runpressed = false
+    if runpressed then
+        Combo.reset(enemy)
+        return "running-with-enemy", player, true
+    end
+
+    holdangle = math.rotangletowards(holdangle, self.holddestangle,
+        enemy.faceturnspeed or (math.pi/64))
+    enemy.holdangle = holdangle
+
+    enemy:accelerateTowardsVel(targetvelx, targetvely)
+    HoldOpponent.updateVelocities(enemy)
+    player.velz = 0
+
+    local velx, vely = enemy.velx, enemy.vely
+    local holdanimation = enemy.state.animation or
+        (velx ~= 0 or vely ~= 0) and "holdwalk" or "hold"
+    Face.faceAngle(enemy, holdangle, holdanimation)
+
+    local opponentfaceangle = holdangle
+    if not self.isfrombehind then
+        opponentfaceangle =  (opponentfaceangle + math.pi)
+    end
+    Face.faceAngle(player, opponentfaceangle, player.state.animation or "Hurt")
+end
+
+function EnemyHold:timeout()
+    local enemy = self.character
+    local player = enemy.heldopponent
+    if player then
+        StateMachine.start(player, "breakaway", enemy)
+    end
+    return "breakaway", player
+end
+
+return EnemyHold
---
+++ game/code/Dragontail/Character/Behavior/Player/Held.lua
@@ -0,0 +1,50 @@
+local Behavior = require "Dragontail.Character.Behavior"
+local Guard    = require "Dragontail.Character.Action.Guard"
+local HoldOpponent = require "Dragontail.Character.Action.HoldOpponent"
+
+---@class PlayerHeld:Behavior
+---@field character Player
+local PlayerHeld = pooledclass(Behavior)
+PlayerHeld._nrec = Behavior._nrec + 1
+
+function PlayerHeld:start(holder)
+    local player = self.character
+    player:stopAttack()
+    Guard.stopGuarding(player)
+    player.velx, player.vely = 0, 0
+    self.holdtime = holder.holdstrength or player.timetobreakhold or 120
+end
+
+function PlayerHeld:fixedupdate()
+    local player = self.character
+    local holder = player.heldby
+
+    if not holder or not HoldOpponent.isHolding(holder, player) then
+        return "walk"
+    end
+
+    local strugglex, struggley = player:getParryVector()
+    if strugglex and struggley then
+        self.holdtime = self.holdtime - 1
+        player.velx = player.velx + strugglex*4
+        player.vely = player.vely + struggley*4
+    end
+    if player:consumeActionRecentlyPressed("attack") then
+        self.holdtime = self.holdtime - 1
+        player.velz = player.velz + 2
+    end
+    if player:consumeActionRecentlyPressed("sprint") then
+        self.holdtime = self.holdtime - 1
+        player.velz = player.velz + 2
+    end
+    if player:consumeActionRecentlyPressed("fly") then
+        self.holdtime = self.holdtime - 1
+        player.velz = player.velz + 2
+    end
+
+    if self.holdtime <= 0 then
+        return "breakaway"
+    end
+end
+
+return PlayerHeld
