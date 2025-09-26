db20798cd7ecf6ad63cdd6e424e4042c70c74d22
IoriBranford <ioribranford@gmail.com>
2025-09-26 00:48:06 -0700

    Crystal spike sprites

---
+++ game/data/sprites/banditcave/crystal-spikes.tsx
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="crystal-spikes" tilewidth="32" tileheight="32" tilecount="32" columns="8" objectalignment="bottom">
+ <tileoffset x="0" y="4"/>
+ <image source="crystal-spikes.png" width="256" height="128"/>
+ <tile id="0" type="spike-small"/>
+ <tile id="1" type="spike-medium"/>
+ <tile id="2" type="spike-large"/>
+ <tile id="3" type="spike-small"/>
+ <tile id="4" type="spike-small"/>
+ <tile id="5" type="spike-large"/>
+ <tile id="6" type="spike-medium"/>
+ <tile id="7" type="spike-small"/>
+ <tile id="8" type="spike-small"/>
+ <tile id="9" type="spike-medium"/>
+ <tile id="10" type="spike-large"/>
+ <tile id="11" type="spike-medium"/>
+ <tile id="12" type="spike-medium"/>
+ <tile id="13" type="spike-large"/>
+ <tile id="14" type="spike-medium"/>
+ <tile id="15" type="spike-small"/>
+ <tile id="16" type="spike-small"/>
+ <tile id="17" type="spike-large"/>
+ <tile id="18" type="spike-medium"/>
+ <tile id="19" type="spike-small"/>
+ <tile id="20" type="spike-small"/>
+ <tile id="21" type="spike-medium"/>
+ <tile id="22" type="spike-large"/>
+ <tile id="23" type="spike-small"/>
+ <tile id="24" type="spike-medium"/>
+ <tile id="25" type="spike-large"/>
+ <tile id="26" type="spike-medium"/>
+ <tile id="27" type="spike-small"/>
+ <tile id="28" type="spike-small"/>
+ <tile id="29" type="spike-medium"/>
+ <tile id="30" type="spike-large"/>
+ <tile id="31" type="spike-medium"/>
+</tileset>
