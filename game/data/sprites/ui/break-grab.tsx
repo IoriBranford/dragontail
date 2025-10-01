168dedcda222e2fa550e2a0796eed724548ecab1
IoriBranford <ioribranford@gmail.com>
2025-10-01 13:49:13 -0700

    Break grab prompt sprites

---
+++ game/data/sprites/ui/break-grab.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="break-grab" tilewidth="32" tileheight="32" tilecount="4" columns="4" objectalignment="bottom">
+ <image source="break-grab.png" width="128" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="x"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="y"/>
+  </properties>
+  <animation>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
