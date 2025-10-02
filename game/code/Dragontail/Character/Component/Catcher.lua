1c76b2cb54184d726102721fd03cd9698b77730a
IoriBranford <ioribranford@gmail.com>
2025-10-02 16:03:21 -0700

    Make catching less lenient
    - Remove extra check against projectile's next position, which breaks if projectile speed is high
    - Default catch arc to 45 degrees around catch vector

--- game/code/Dragontail/Character/Component/Catcher.lua
+++ game/code/Dragontail/Character/Component/Catcher.lua
@@ -5,7 +5,7 @@ local Catcher = {}
 
 function Catcher:findCharacterToCatch(characters, catchdirx, catchdiry)
     local catchradius = (self.catchradius or 20)
-    local coscatcharc = math.cos(self.catcharc or (math.pi/2))
+    local coscatcharc = math.cos(self.catcharc or (math.pi/4))
     local x, y, z = self.x, self.y, self.z
     local ztop = z + self.bodyheight + catchradius/2
     for _, character in ipairs(characters) do
@@ -21,12 +21,6 @@ function Catcher:findCharacterToCatch(characters, catchdirx, catchdiry)
             local toprojx = character.x - x
             local toprojy = character.y - y
             local d = math.dot(catchdirx, catchdiry, toprojx, toprojy)
-            if mindot <= d and d <= catchprojradius then
-                return character
-            end
-            local toprojx2 = toprojx + character.velx
-            local toprojy2 = toprojy + character.vely
-            d = math.dot(catchdirx, catchdiry, toprojx2, toprojy2)
             if mindot <= d and d <= catchprojradius then
                 return character
             end
