53d0656835b43b8ba8b41deeb01b74ebe279ec46
IoriBranford <ioribranford@gmail.com>
2025-10-11 18:11:41 +0800

    Alternate tree2A tileset with the whole tree as a tile

---
+++ game/data/tilesets/veg/grassland/tree2A_ss_obj.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="tree2A_ss_obj" tilewidth="128" tileheight="160" tilecount="12" columns="4" objectalignment="bottom">
+ <tileoffset x="-12" y="20"/>
+ <image source="tree2A_ss.png" width="512" height="512"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="150"/>
+   <frame tileid="1" duration="150"/>
+   <frame tileid="2" duration="150"/>
+   <frame tileid="3" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
