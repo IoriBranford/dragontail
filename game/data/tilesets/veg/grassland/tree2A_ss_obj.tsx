bccd72dcfa1810ae657c58baa5e27fd24f75f6d1
IoriBranford <ioribranford@gmail.com>
2025-10-13 12:43:24 +0800

    Aseprites of various color trees which will drop different types of fruit

--- game/data/tilesets/veg/grassland/tree2A_ss_obj.tsx
+++ game/data/tilesets/veg/grassland/tree2A_ss_obj.tsx
@@ -1,13 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="tree2A_ss_obj" tilewidth="128" tileheight="160" tilecount="12" columns="4" objectalignment="bottom">
+<tileset version="1.11" tiledversion="1.11.2" name="tree2A_ss_obj" tilewidth="128" tileheight="160" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="-12" y="20"/>
- <image source="tree2A_ss.png" width="512" height="512"/>
- <tile id="0">
-  <animation>
-   <frame tileid="0" duration="150"/>
-   <frame tileid="1" duration="150"/>
-   <frame tileid="2" duration="150"/>
-   <frame tileid="3" duration="150"/>
-  </animation>
- </tile>
+ <image source="tree2A_ss.ase" width="128" height="160"/>
 </tileset>
---
+++ game/data/tilesets/veg/grassland/tree2B_ss_obj.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="tree2B_ss_obj" tilewidth="128" tileheight="160" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="-12" y="20"/>
+ <image source="tree2B_ss.ase" width="128" height="160"/>
+</tileset>
