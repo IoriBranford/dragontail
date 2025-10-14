696437813dd1340563370cfb0372b0a74b237a8d
IoriBranford <ioribranford@gmail.com>
2025-10-14 13:42:44 +0800

    Make spikefruit aseprite with inflight, onground, inhand, hanging, embedded animations

---
+++ game/data/sprites/weapons/spikefruit-onground.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="spikefruit-onground" tilewidth="16" tileheight="16" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="2"/>
+ <image source="spikefruit.ase" width="16" height="16"/>
+</tileset>
--- game/data/sprites/weapons/spikefruit.tsx
+++ game/data/sprites/weapons/spikefruit.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.11" tiledversion="1.11.2" name="spikefruit" tilewidth="16" tileheight="16" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="2"/>
- <image source="spikefruit.png" width="16" height="16"/>
+ <image source="spikefruit.ase" width="16" height="16"/>
 </tileset>
