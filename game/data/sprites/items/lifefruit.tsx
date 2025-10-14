a75bbf10b5afaade24af5fd0c108048dc32f3cc5
IoriBranford <ioribranford@gmail.com>
2025-10-14 15:55:46 +0800

    Life fruit sprite

---
+++ game/data/sprites/items/lifefruit.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="lifefruit" tilewidth="16" tileheight="16" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="2"/>
+ <image source="lifefruit.png" width="16" height="16"/>
+</tileset>
