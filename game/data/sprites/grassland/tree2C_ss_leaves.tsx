9627691bb69a48181522b04f7ce368475423752b
IoriBranford <ioribranford@gmail.com>
2025-10-22 02:10:19 -0700

    Adjust spikefruit tree leaves offset to ensure they will cover hanging fruits

--- game/data/sprites/grassland/tree2C_ss_leaves.tsx
+++ game/data/sprites/grassland/tree2C_ss_leaves.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.11" tiledversion="1.11.2" name="tree2C_ss_leaves" tilewidth="128" tileheight="96" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="-12" y="28"/>
+ <tileoffset x="-12" y="12"/>
  <image source="tree2C_ss_leaves.ase" width="128" height="96"/>
 </tileset>
