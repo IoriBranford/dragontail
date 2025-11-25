e1e2aead2b8ad3651c629916d40eba4d34c0c2d6
IoriBranford <ioribranford@gmail.com>
2025-11-24 17:07:01 -0800

    Remove old muscle bandit sprite from game

--- game/data/sprites/bandit/muscle.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="muscle" tilewidth="128" tileheight="128" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="40"/>
- <image source="muscle.ase" width="128" height="128"/>
-</tileset>
