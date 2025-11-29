1722fefb5d6cc68be9c41f5e8b2b8f7ba8381092
IoriBranford <ioribranford@gmail.com>
2025-11-28 17:25:13 -0800

    Set pepper plant sprite origins, add tilesets for placing them in maps

---
+++ game/data/sprites/items/pepper-plant.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="pepper-plant" tilewidth="32" tileheight="32" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="2"/>
+ <image source="pepper-plant.ase" width="32" height="32"/>
+ <tile id="0" type="pepper-plant"/>
+</tileset>
---
+++ game/data/sprites/items/peppers-on-plant.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="peppers-on-plant" tilewidth="32" tileheight="32" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="2"/>
+ <image source="peppers-on-plant.ase" width="32" height="32"/>
+ <tile id="0" type="mana-peppers-on-plant"/>
+</tileset>
