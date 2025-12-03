d475f50bef4e8b44cb81378bac15083832ee9d6f
IoriBranford <ioribranford@gmail.com>
2025-12-03 14:41:36 -0800

    Raw LPC ship tileset

---
+++ rawdata/tilesets/lpc-ship/container.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="container" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
+ <image source="_preview_only_container.png" trans="ff00ff" width="512" height="2048"/>
+</tileset>
---
+++ rawdata/tilesets/lpc-ship/ship.tsx
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="ship" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="lpc-ship.png" trans="ff00ff" width="1024" height="1024"/>
+ <tile id="768">
+  <animation>
+   <frame tileid="768" duration="200"/>
+   <frame tileid="864" duration="200"/>
+   <frame tileid="896" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="769">
+  <animation>
+   <frame tileid="769" duration="200"/>
+   <frame tileid="865" duration="200"/>
+   <frame tileid="897" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="802">
+  <animation>
+   <frame tileid="802" duration="200"/>
+   <frame tileid="866" duration="200"/>
+   <frame tileid="898" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="803">
+  <animation>
+   <frame tileid="803" duration="200"/>
+   <frame tileid="867" duration="200"/>
+   <frame tileid="899" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="816">
+  <animation>
+   <frame tileid="816" duration="200"/>
+   <frame tileid="848" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="817">
+  <animation>
+   <frame tileid="817" duration="200"/>
+   <frame tileid="849" duration="200"/>
+   <frame tileid="881" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="836">
+  <animation>
+   <frame tileid="836" duration="200"/>
+   <frame tileid="868" duration="200"/>
+   <frame tileid="900" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="837">
+  <animation>
+   <frame tileid="837" duration="200"/>
+   <frame tileid="869" duration="200"/>
+   <frame tileid="901" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="838">
+  <animation>
+   <frame tileid="838" duration="200"/>
+   <frame tileid="870" duration="200"/>
+   <frame tileid="902" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="839">
+  <animation>
+   <frame tileid="839" duration="200"/>
+   <frame tileid="871" duration="200"/>
+   <frame tileid="903" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="840">
+  <animation>
+   <frame tileid="840" duration="200"/>
+   <frame tileid="872" duration="200"/>
+   <frame tileid="904" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="852" probability="0.1"/>
+ <tile id="853" probability="0.1"/>
+ <tile id="854" probability="0.1"/>
+</tileset>
