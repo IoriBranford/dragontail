429e8b1122330894ff632c349f56272459963878
IoriBranford <ioribranford@gmail.com>
2025-12-17 14:37:58 -0800

    Restore hold breaking immediately if grabbing opponent within his guard

--- game/code/Dragontail/Character/Behavior/Fighter/Grabbing.lua
+++ game/code/Dragontail/Character/Behavior/Fighter/Grabbing.lua
@@ -1,6 +1,7 @@
 local Behavior = require "Dragontail.Character.Behavior"
 local HoldOpponent = require "Dragontail.Character.Component.HoldOpponent"
 local Face         = require "Dragontail.Character.Component.Face"
+local Guard        = require "Dragontail.Character.Component.Guard"
 
 ---@class Grabbing:Behavior
 ---@field character Fighter
@@ -8,7 +9,17 @@ local Grabbing = pooledclass(Behavior)
 
 function Grabbing:start(grabbed)
     local fighter = self.character
+    local dx, dy = grabbed.x - fighter.x, grabbed.y - fighter.y
+    local guarded
+    if dx == 0 and dy == 0 then
+        guarded = false
+    else
+        guarded = Guard.isUnitVectorAgainstGuardArc(grabbed, math.norm(dx, dy))
+    end
     HoldOpponent.startHolding(fighter, grabbed)
+    if guarded then
+        fighter.holdstrength = 0
+    end
 end
 
 function Grabbing:fixedupdate()
