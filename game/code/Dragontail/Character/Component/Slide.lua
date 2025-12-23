4c67b832049e04ae20f22f15d55f9cba77c47019
IoriBranford <ioribranford@gmail.com>
2025-12-22 17:57:08 -0800

    Slide.GetSlideSpeedForDistance

--- game/code/Dragontail/Character/Component/Slide.lua
+++ game/code/Dragontail/Character/Component/Slide.lua
@@ -6,6 +6,21 @@ function Slide.GetSlideDistance(speed, decel)
     return speed * (speed+decel) / (2*decel)
 end
 
+function Slide.GetSlideSpeedForDistance(dist, decel)
+    -- dist = (accel*time*time)/2 + speed0*time
+    -- dist - (accel*time*time)/2 = speed0*time
+    -- dist/time - (accel*time)/2 = speed0
+    -- 0 = speed0 + accel/time
+    -- speed0 = -accel/time
+    -- time = -speed0/accel
+    -- -dist*accel/speed0 + speed0/2 = speed0
+    -- -dist*accel/speed0 = speed0/2
+    -- -2*dist*accel/speed0 = speed0
+    -- -2*dist*accel = speed0*speed0
+    -- speed0 = sqrt(-2*dist*accel)
+    return math.sqrt(2*dist*decel)
+end
+
 function Slide:updateSlideSpeed(angle, speed, decel)
     decel = decel or 1
     self.velx = speed * math.cos(angle)
