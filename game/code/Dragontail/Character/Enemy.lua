3587d35f13c1e677246a55537d66d75c981902f0
IoriBranford <ioribranford@gmail.com>
2025-12-30 15:19:08 -0800

    Wait until player is vulnerable in attackIfAmmoElseLeave

--- game/code/Dragontail/Character/Enemy.lua
+++ game/code/Dragontail/Character/Enemy.lua
@@ -283,6 +283,7 @@ function Enemy:attackIfAmmoElseLeave()
             Face.faceVector(self, raycast.dx, raycast.dy, "Stand")
             hitcharacter = Characters.castRay3(raycast, self)
         until self:isOnCamera(self.camera) and hitcharacter
+            and hitcharacter.canbeattacked
             and CollisionMask.testAll(hitcharacter.bodyinlayers, "Player") ~= 0
         self.ammo = self.ammo - 1
         return attacktype
