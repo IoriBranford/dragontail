f494c9242b90a1030a66531a715861c714cda7b2
IoriBranford <ioribranford@gmail.com>
2025-12-29 17:45:43 -0800

    Muscle interception logic

--- game/code/Dragontail/Character/Enemy/MuscleBandit.lua
+++ game/code/Dragontail/Character/Enemy/MuscleBandit.lua
@@ -6,6 +6,7 @@ local DirectionalAnimation = require "Dragontail.Character.Component.Directional
 local Body                 = require "Dragontail.Character.Component.Body"
 local CollisionMask        = require "Dragontail.Character.Component.Body.CollisionMask"
 local Face                 = require "Dragontail.Character.Component.Face"
+local Slide        = require "Dragontail.Character.Component.Slide"
 
 ---@class MuscleBandit:Enemy
 local MuscleBandit = class(Enemy)
@@ -27,15 +28,17 @@ function MuscleBandit:duringApproach(opponent)
     if not self:isCylinderFullyOnCamera(self.camera) then return end
     if self.weaponinhand then return end
 
-    local nextstate, nextstatearg
     local time = 10
     local sightarc = self.sightarc or (math.pi/4)
+    local maxdodgespeed, dodgedecel = self.dodgespeed or 7, self.dodgedecel or .25
+    local selfx, selfy = self.x, self.y
+    local dodgevelx, dodgevely, dodgefaceangle
+
     local function isComing(them)
         if Face.isObjectInSight(self, them, sightarc)
         and Body.isInTheirWay(self, them, time) then
             Face.faceObject(self, them)
-            nextstate = "catchReady"
-            return "break"
+            return them
         end
     end
     local function isThrownEnemyComing(them)
@@ -44,8 +47,7 @@ function MuscleBandit:duringApproach(opponent)
         and Face.isObjectInSight(self, them, sightarc)
         and Body.isInTheirWay(self, them, time) then
             Face.faceObject(self, them)
-            nextstate = "catchReady"
-            return "break"
+            return them
         end
     end
     local function isCloseEnoughToGrab(them)
@@ -58,23 +60,64 @@ function MuscleBandit:duringApproach(opponent)
         local radii = self.bodyradius + cr
         if math.distsq(self.x, self.y, cx, cy) <= radii*radii then
             if cz <= self.z + self.bodyheight and cz + ch >= self.z then
-                nextstate = "grab"
-                nextstatearg = them
-                return "break"
+                return them
             end
         end
     end
 
-    Characters.search("players", isComing)
-    if nextstate then return nextstate, nextstatearg end
-    Characters.search("projectiles", isComing)
-    if nextstate then return nextstate, nextstatearg end
-    Characters.search("enemies", isThrownEnemyComing)
-    if nextstate then return nextstate, nextstatearg end
-    Characters.search("container", isThrownEnemyComing)
-    if nextstate then return nextstate, nextstatearg end
-    Characters.search("container", isCloseEnoughToGrab)
-    if nextstate then return nextstate, nextstatearg end
+    local function isInterceptable(them)
+        if not Face.isObjectInSight(self, them, sightarc) then return end
+        if them.z + them.bodyheight < self.z
+        or self.z + self.bodyheight < them.z then return end
+        local theirvelx, theirvely = them.velx, them.vely
+        if theirvelx == 0 and theirvely == 0 then return end
+        local theirx, theiry = them.x, them.y
+        local theirdist = math.dist(selfx, selfy, theirx, theiry)
+        local theirspeed = math.len(theirvelx, theirvely)
+        if math.dot(theirvelx, theirvely, selfx - theirx, selfy - theiry)
+        < theirdist*theirspeed*.5 then
+            return
+        end
+        local theirx2 = theirx + theirvelx*time
+        local theiry2 = theiry + theirvely*time
+        local targetx, targety = math.projpointsegment(selfx, selfy, theirx, theiry, theirx2, theiry2)
+        local targetdist = math.dist(selfx, selfy, targetx, targety)
+        local dodgespeed = Slide.GetSlideSpeedForDistance(targetdist, dodgedecel)
+        if dodgespeed > maxdodgespeed then return end
+
+        if targetdist > 0 then
+            dodgevelx = (targetx-selfx)*dodgespeed/targetdist
+            dodgevely = (targety-selfy)*dodgespeed/targetdist
+            -- Characters.spawn({
+            --     x = targetx-1, y = targety-1, shape = "ellipse",
+            --     width = 2, height = 2,
+            --     color = 0xFFFF0000
+            -- })
+        end
+        dodgefaceangle = math.atan2(-theirvely, -theirvelx)
+        return them
+    end
+    local function isThrownInterceptable(them)
+        return them.thrower
+            and them.thrower.team == "players"
+            and isInterceptable(them)
+    end
+
+    local whotocatch = Characters.search("players", isInterceptable)
+        or Characters.search("projectiles", isInterceptable)
+        or Characters.search("enemies", isThrownInterceptable)
+        or Characters.search("container", isThrownInterceptable)
+    if whotocatch then
+        if dodgevelx and dodgevely then
+            self.velx, self.vely = dodgevelx, dodgevely
+        end
+        if dodgefaceangle then
+            Face.faceAngle(self, dodgefaceangle)
+        end
+        return "catchReady"
+    end
+    local whattograb = Characters.search("container", isCloseEnoughToGrab)
+    if whattograb then return "grab", whattograb end
 end
 
 function MuscleBandit:decideNextAttack()
