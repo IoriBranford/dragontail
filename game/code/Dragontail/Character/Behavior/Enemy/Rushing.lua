1d7ca74d5b8ef2c504b49bffe10b869eb9f45439
IoriBranford <ioribranford@gmail.com>
2026-01-05 16:15:53 -0800

    Behavior for rushing at a target

---
+++ game/code/Dragontail/Character/Behavior/Enemy/Rushing.lua
@@ -0,0 +1,64 @@
+local Behavior = require "Dragontail.Character.Behavior"
+local Face     = require "Dragontail.Character.Component.Face"
+local Body     = require "Dragontail.Character.Component.Body"
+
+---@class Rushing:Behavior
+---@field character Enemy
+local Rushing = pooledclass(Behavior)
+Rushing._nrec = 2
+
+---@param target Character
+function Rushing:start(target, nextstate)
+    local enemy = self.character
+    if nextstate then
+        enemy.nextstate = nextstate
+    else
+        nextstate = enemy.nextstate
+    end
+    self.target = target or enemy.opponents[1]
+    self.t = 0
+end
+
+---@return string? nextstate
+---@return any ...
+function Rushing:fixedupdate()
+    local enemy = self.character
+    local target = self.target
+    local x, y = enemy.x, enemy.y
+    local dsq = math.distsq(x, y, target.x, target.y)
+    local speed = enemy.speed or 1
+    local nextstate = enemy.statetable[enemy.nextstate]
+    local closeenoughdist = math.max(speed,
+        nextstate and nextstate.maxtargetdist or 1)
+    if dsq <= closeenoughdist*closeenoughdist then
+        return self:timeout(enemy.nextstate)
+    end
+
+    local faceangle = Face.turnTowardsObject(enemy, target, nil,
+        enemy.state.animation, enemy.state.frame1, enemy.state.loopframe)
+    local velx = math.cos(faceangle) * speed
+    local vely = math.sin(faceangle) * speed
+    Body.forceTowardsVelXY(enemy, velx, vely, enemy.accel)
+
+    local t = self.t + 1
+    enemy:makePeriodicAfterImage(t, enemy.afterimageinterval)
+    self.t = t
+end
+
+---@return string? nextstate
+---@return any ...
+function Rushing:interrupt(nextstate, ...)
+    return nextstate, ...
+end
+
+---@return string? nextstate
+---@return any ...
+function Rushing:timeout(nextstate, ...)
+    return nextstate, ...
+end
+
+---@param fixedfrac number
+function Rushing:draw(fixedfrac)
+end
+
+return Rushing
