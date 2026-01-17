a8ec29f0829b5527b0eaa6f4d432a358ec7d8a0c
IoriBranford <ioribranford@gmail.com>
2026-01-16 18:00:34 -0800

    GroundSlam behavior

---
+++ game/code/Dragontail/Character/Behavior/Player/GroundSlam.lua
@@ -0,0 +1,75 @@
+local Behavior = require "Dragontail.Character.Behavior"
+local HoldOpponent = require "Dragontail.Character.Component.HoldOpponent"
+local Attacker     = require "Dragontail.Character.Component.Attacker"
+local StateMachine = require "Dragontail.Character.Component.StateMachine"
+
+---@class GroundSlam:Behavior
+---@field character Player
+local GroundSlam = pooledclass(Behavior)
+GroundSlam._nrec = 1
+
+function GroundSlam:_init(character)
+    self.character = character
+end
+
+---@param ... any
+function GroundSlam:start(...)
+    local player = self.character
+    player.holddist = 0
+    player.holdheight = -8
+    -- Attacker.startAttack(player, math.pi/2)
+    local heldenemy = player.heldopponent
+    if heldenemy then
+        heldenemy:changeAnimation("Down")
+    end
+end
+
+---@return string? nextstate
+---@return any ...
+function GroundSlam:fixedupdate()
+    local player = self.character
+    local heldenemy = player.heldopponent
+    local landed = heldenemy and heldenemy.z <= heldenemy.floorz
+        or player.z <= player.floorz
+
+    if landed then
+        if heldenemy then
+            StateMachine.start(heldenemy, "groundSlammed", player)
+        end
+        -- local jumpangle = player.faceangle
+        -- player.velx = math.cos(jumpangle)*4
+        -- player.vely = math.sin(jumpangle)*4
+        player.velz = 4
+        HoldOpponent.stopHolding(player, heldenemy)
+        return "jump"
+        -- player.hitstun = heldenemy.hurtstun
+        -- player.statetime = 1
+        -- player.nextstate = "jump"
+    end
+
+    HoldOpponent.updateVelocities(player)
+end
+
+---@return string? nextstate
+---@return any ...
+function GroundSlam:interrupt(nextstate, ...)
+    local player = self.character
+    player.holddist = nil
+    player.holdheight = nil
+    return nextstate, ...
+end
+
+---@return string? nextstate
+---@return any ...
+function GroundSlam:timeout(nextstate, ...)
+    local player = self.character
+    player.holddist = nil
+    player.holdheight = nil
+    return nextstate, ...
+end
+
+---@param fixedfrac number
+function GroundSlam:draw(fixedfrac)
+end
+
+return GroundSlam
