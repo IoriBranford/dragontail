b7226216aae96a404f0b17f8fb548541688f9389
IoriBranford <ioribranford@gmail.com>
2026-01-23 12:38:41 -0800

    Xbox button sprites

---
+++ game/data/tilesets/ui/Gamepad Spritesheets/Xbox.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="Xbox" tilewidth="14" tileheight="14" tilecount="20" columns="20" objectalignment="center">
+ <image source="Xbox.png" width="280" height="15"/>
+</tileset>
