6f5f3deb74fea0fe13b7bb504cd50e9fb6884be9
IoriBranford <ioribranford@gmail.com>
2026-04-18 16:33:13 -0700

    Create player guard impact behavior

---
+++ game/code/Dragontail/Character/Behavior/Player/GuardHit.lua
@@ -0,0 +1,31 @@
+local Guard    = require "Dragontail.Character.Component.Guard"
+local Behavior = require "Dragontail.Character.Behavior"
+local Invulnerability = require "Dragontail.Character.Component.Invulnerability"
+local Face            = require "Dragontail.Character.Component.Face"
+
+---@class PlayerGuardHit:Behavior
+---@field character Enemy
+local PlayerGuardHit = pooledclass(Behavior)
+
+---@param hit AttackHit
+function PlayerGuardHit:start(hit)
+    local player = self.character
+    Guard.standardImpact(player, hit)
+    local guardangle = assert(hit.guardangle)
+    Guard.startGuarding(player, guardangle)
+    Face.faceAngle(player, guardangle + math.pi, player.state.animation, player.state.frame1, player.state.loopframe)
+end
+
+function PlayerGuardHit:interrupt(...)
+    local player = self.character
+    Invulnerability.giveInvuln(player, player.guardinvulntime or 120)
+    return ...
+end
+
+function PlayerGuardHit:timeout(...)
+    local player = self.character
+    Invulnerability.giveInvuln(player, player.guardinvulntime or 120)
+    return ...
+end
+
+return PlayerGuardHit
