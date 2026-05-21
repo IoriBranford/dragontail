0f7d90452d7dec24d4f5f46c92746702fb8ad833
IoriBranford <ioribranford@gmail.com>
2026-05-20 17:45:54 -0700

    Create individual arrowhead sprite for turning GO arrows into progress meter

---
+++ game/data/sprites/ui/go-arrowhead.lua
@@ -0,0 +1,895 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.1",
+  name = "go-arrowhead",
+  class = "",
+  tilewidth = 16,
+  tileheight = 16,
+  spacing = 0,
+  margin = 0,
+  columns = 16,
+  image = "go-arrowhead.png",
+  imagewidth = 256,
+  imageheight = 16,
+  objectalignment = "center",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 16,
+    height = 16
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 16,
+  tiles = {
+    {
+      id = 0,
+      properties = {
+        ["name"] = "full"
+      },
+      animation = {
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 1,
+      animation = {
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 2,
+      animation = {
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 3,
+      animation = {
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 4,
+      animation = {
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 5,
+      animation = {
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 6,
+      animation = {
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 7,
+      animation = {
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 8,
+      animation = {
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 9,
+      animation = {
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 10,
+      animation = {
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 11,
+      animation = {
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 12,
+      animation = {
+        {
+          tileid = 12,
+          duration = 50
+        },
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 13,
+      animation = {
+        {
+          tileid = 13,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 6,
+          duration = 50
+        },
+        {
+          tileid = 7,
+          duration = 50
+        },
+        {
+          tileid = 8,
+          duration = 50
+        },
+        {
+          tileid = 9,
+          duration = 50
+        },
+        {
+          tileid = 10,
+          duration = 50
+        },
+        {
+          tileid = 11,
+          duration = 50
+        },
+        {
+          tileid = 12,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 14,
+      properties = {
+        ["name"] = "empty"
+      }
+    }
+  }
+}
---
+++ game/data/sprites/ui/go-arrowhead.tsx
@@ -0,0 +1,264 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.1" name="go-arrowhead" tilewidth="16" tileheight="16" tilecount="16" columns="16" objectalignment="center">
+ <image source="go-arrowhead.png" width="256" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="full"/>
+  </properties>
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="50"/>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="6" duration="50"/>
+   <frame tileid="7" duration="50"/>
+   <frame tileid="8" duration="50"/>
+   <frame tileid="9" duration="50"/>
+   <frame tileid="10" duration="50"/>
+   <frame tileid="11" duration="50"/>
+   <frame tileid="12" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="empty"/>
+  </properties>
+ </tile>
+</tileset>
