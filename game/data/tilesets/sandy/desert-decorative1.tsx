12f107c50fcc957df2e3a1f37efb2c94ee14cd83
IoriBranford <ioribranford@gmail.com>
2026-06-29 14:44:01 -0700

    Desert/beach graphic assets by Szadi art

---
+++ game/data/tilesets/sandy/desert-decorative1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="desert-decorative1" tilewidth="32" tileheight="32" tilecount="960" columns="40">
+ <image source="desert-decorative1.png" width="1280" height="768"/>
+</tileset>
---
+++ game/data/tilesets/sandy/desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="desert" tilewidth="32" tileheight="32" tilecount="2070" columns="46">
+ <image source="desert.png" width="1472" height="1440"/>
+</tileset>
