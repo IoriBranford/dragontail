9906eb7f72a854c65cfcd6ff65cf58c236123eee
IoriBranford <ioribranford@gmail.com>
2026-07-09 08:55:25 -0700

    Finish bulk export tilesets script

--- game/data/extensions/bulkExportTilesets.js
+++ game/data/extensions/bulkExportTilesets.js
@@ -2,24 +2,29 @@
 
 const openAssets = new Set();
 
-const exportTilesetFile = (fileName) => {
+const exportFile = (fileName) => {
     const tileset = tiled.open(fileName);
+    if (!tileset) return;
+
     tiled.trigger("Export");
     tileset.save();
+    if (!openAssets.has(tileset.fileName)) {
+        tiled.close(tileset);
+    };
 }
 
-const exportTilesetFiles = (tilesetFiles) => {
-    tilesetFiles.forEach((fileName) => {
-        const tileset = tiled.open(fileName);
-        if (tileset) {
-            if (tileset.isTileset) {
-                exportTilesetFile(tileset.fileName);
-            }
-            if (!openAssets.has(tileset.fileName)) {
-                tiled.close(tileset);
-            };
-        }
-    });
+const exportDir = (dir, recurse) => {
+    tiled.log(dir)
+    File.directoryEntries(dir, File.Files, File.Name)
+        .filter((s) => FileInfo.suffix(s) == "tsx")
+        .map((s) => FileInfo.joinPaths(dir, s))
+        .forEach(exportFile);
+
+    if (recurse) {
+        File.directoryEntries(dir, File.Dirs | File.NoDotAndDotDot, File.Name)
+            .map((s) => FileInfo.joinPaths(dir, s))
+            .forEach((s) => exportDir(s, recurse));
+    }
 }
 
 const setup = () => {
@@ -35,86 +40,29 @@ const setup = () => {
     }
 }
 
-const bulkExportTilesets = () => {
+const BulkExportTilesets = () => {
     const {startDir} = setup();
-
-    const fileNames = tiled.promptOpenFiles(startDir, "Tilesets (*.tsx)");
-    if (fileNames.length <= 0)
-        return;
-
-    exportTilesetFiles(fileNames);
+    tiled.promptOpenFiles(startDir, "Tilesets (*.tsx)")
+        .forEach(exportFile);
 }
 
-const bulkExportTilesetsDir = () => {
+const BulkExportTilesetsDir = (recurse) => {
     const {startDir} = setup();
-
     const dir = tiled.promptDirectory(startDir, "Export all tilesets in directory");
     if (dir.length <= 0)
         return;
-
-    const fileNames = File.directoryEntries(dir, File.Files, File.Name)
-        .filter((s) => FileInfo.suffix(s) == "tsx")
-        .map((s) => FileInfo.joinPaths(dir, s));
-
-    if (fileNames.length <= 0)
-        return;
-    exportTilesetFiles(fileNames);
+    exportDir(dir, recurse);
 }
 
-const bulkExportTilesetsDirRecursive = () => {}
-
-tiled.registerAction("BulkExportTilesets", bulkExportTilesets)
+tiled.registerAction("BulkExportTilesets", BulkExportTilesets)
 .text = "Bulk Export Tilesets...";
-tiled.registerAction("BulkExportTilesetsDir", bulkExportTilesetsDir)
+tiled.registerAction("BulkExportTilesetsDir", BulkExportTilesetsDir)
 .text = "Bulk Export Tilesets in Directory...";
+tiled.registerAction("BulkExportTilesetsRecursive", ()=>BulkExportTilesetsDir(true))
+.text = "Bulk Export Tilesets in Directory Tree...";
 
 tiled.extendMenu("File", [
     {action: "BulkExportTilesets", before: "Reload"},
-    {action: "BulkExportTilesetsDir"}
+    {action: "BulkExportTilesetsDir"},
+    // {action: "BulkExportTilesetsRecursive"}
 ]);
-
-// const exportTilesetsInMaps = (mapFiles) => {
-//     const openAssets = new Set(tiled.openAssets);
-//     const exportedTilesets = new Set();
-    
-//     mapFiles.forEach((fileName) => {
-//         const map = tiled.open(fileName);
-//         if (map && map.isTileMap) {
-//             /*** @type Tileset[] */
-//             const tilesets = map.tilesets;
-//             tilesets.forEach((tileset) => {
-//                 if (!exportedTilesets.has(tileset.fileName)) {
-//                     exportedTilesets.add(tileset.fileName);
-//                     exportTilesetFile(tileset);
-//                     if (!openAssets.has(tileset.fileName)) {
-//                         tiled.close(tileset);
-//                     };
-//                 };
-//             });
-//             tiled.activeAsset = map;
-//             tiled.trigger("Export");
-//             map.save();
-//             if (!openAssets.has(map.fileName)) {
-//                 tiled.close(map);
-//             };
-//         }
-//     });
-// }
-
-// tiled.registerAction("BulkExportMapsTilesets", () => {
-//     const startDir =
-//         tiled.activeAsset && FileInfo.cleanPath(tiled.activeAsset.fileName) ||
-//         tiled.project.folders.length > 0 && tiled.project.folders[0] ||
-//         tiled.project.fileName.length > 0 && FileInfo.cleanPath(tiled.project.fileName) ||
-//         '.';
-
-//     const fileNames = tiled.promptOpenFiles(startDir, "Maps (*.tmx)");
-//     if (fileNames.length <= 0)
-//         return;
-
-//     exportTilesetsInMaps(fileNames);
-// }).text = "Bulk Export All Tilesets in Maps...";
-
-// tiled.extendMenu("File", [
-//     {action: "BulkExportMapsTilesets", before: "Reload"}
-// ]);
