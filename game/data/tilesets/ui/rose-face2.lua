89a7042c8eaddc522ec3818b2c018832bd3b460d
IoriBranford <ioribranford@gmail.com>
2026-07-09 12:37:42 -0700

    New HUD portrait files

---
+++ game/data/tilesets/ui/rose-face2.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  name = "rose-face2",
+  class = "",
+  tilewidth = 60,
+  tileheight = 27,
+  spacing = 0,
+  margin = 0,
+  columns = 1,
+  image = "rose-face2.png",
+  imagewidth = 60,
+  imageheight = 27,
+  objectalignment = "bottom",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 60,
+    height = 27
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 1,
+  tiles = {}
+}
---
+++ game/data/tilesets/ui/rose-face2.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="rose-face2" tilewidth="60" tileheight="27" tilecount="1" columns="1" objectalignment="bottom">
+ <editorsettings>
+  <export target="rose-face2.lua" format="lua"/>
+ </editorsettings>
+ <image source="rose-face2.png" width="60" height="27"/>
+</tileset>
