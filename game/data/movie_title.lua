681247e8cdff0b1be6b862e117b3c16262f4d8e5
IoriBranford <ioribranford@gmail.com>
2026-07-17 05:22:02 -0700

    Create new movie map with title animation

---
+++ game/data/movie_title.lua
@@ -0,0 +1,174 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  class = "",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 30,
+  height = 15,
+  tilewidth = 16,
+  tileheight = 18,
+  nextlayerid = 11,
+  nextobjectid = 3,
+  properties = {
+    ["runphase"] = "Dragontail.MoviePhase"
+  },
+  tilesets = {
+    {
+      name = "title",
+      firstgid = 1,
+      filename = "sprites/ui/title.tsx",
+      exportfilename = "sprites/ui/title.lua"
+    }
+  },
+  layers = {
+    {
+      type = "group",
+      id = 10,
+      name = "TitleHit",
+      class = "Tiled.Movie",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {
+        ["hitsound"] = "sounds/combat/heavyswingandhit.ogg",
+        ["script"] = "Dragontail.Movie.RoseHitEnemy"
+      },
+      layers = {
+        {
+          type = "imagelayer",
+          image = "sprites/ui/title/illust/bg.png",
+          id = 2,
+          name = "bg",
+          class = "",
+          visible = true,
+          opacity = 1,
+          offsetx = 0,
+          offsety = 0,
+          parallaxx = 1,
+          parallaxy = 1,
+          repeatx = false,
+          repeaty = false,
+          properties = {}
+        },
+        {
+          type = "group",
+          id = 7,
+          name = "fg",
+          class = "",
+          visible = true,
+          opacity = 1,
+          offsetx = -92,
+          offsety = -130.5,
+          parallaxx = 1,
+          parallaxy = 1,
+          properties = {},
+          layers = {
+            {
+              type = "imagelayer",
+              image = "sprites/ui/title/illust/07 Thug_L.png",
+              id = 3,
+              name = "enemy",
+              class = "",
+              visible = true,
+              opacity = 1,
+              offsetx = 68,
+              offsety = -27,
+              parallaxx = 1,
+              parallaxy = 1,
+              repeatx = false,
+              repeaty = false,
+              properties = {}
+            },
+            {
+              type = "imagelayer",
+              image = "sprites/ui/title/illust/08 effects L.png",
+              id = 5,
+              name = "hit",
+              class = "",
+              visible = true,
+              opacity = 1,
+              offsetx = 20,
+              offsety = 9,
+              parallaxx = 1,
+              parallaxy = 1,
+              repeatx = false,
+              repeaty = false,
+              properties = {}
+            },
+            {
+              type = "imagelayer",
+              image = "sprites/ui/title/illust/09 Rose.png",
+              id = 4,
+              name = "Rose",
+              class = "",
+              visible = true,
+              opacity = 1,
+              offsetx = 0,
+              offsety = 0,
+              parallaxx = 1,
+              parallaxy = 1,
+              repeatx = false,
+              repeaty = false,
+              properties = {}
+            },
+            {
+              type = "imagelayer",
+              image = "sprites/ui/title/illust/10 swipe.png",
+              id = 6,
+              name = "swing",
+              class = "",
+              visible = true,
+              opacity = 1,
+              offsetx = -68,
+              offsety = 4.5,
+              parallaxx = 1,
+              parallaxy = 1,
+              repeatx = false,
+              repeaty = false,
+              properties = {}
+            }
+          }
+        },
+        {
+          type = "objectgroup",
+          draworder = "topdown",
+          id = 8,
+          name = "directions",
+          class = "",
+          visible = true,
+          opacity = 1,
+          offsetx = 0,
+          offsety = 0,
+          parallaxx = 1,
+          parallaxy = 1,
+          properties = {},
+          objects = {
+            {
+              id = 1,
+              name = "path",
+              type = "",
+              shape = "polyline",
+              x = 520,
+              y = 36,
+              width = 0,
+              height = 0,
+              rotation = 0,
+              opacity = 1,
+              visible = true,
+              polyline = {
+                { x = 0, y = 0 },
+                { x = -520, y = -36 }
+              },
+              properties = {}
+            }
+          }
+        }
+      }
+    }
+  }
+}
