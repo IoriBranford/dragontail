f339119100332248f78110621e6975341d9006a5
IoriBranford <ioribranford@gmail.com>
2026-07-20 12:31:41 -0700

    More small decoration sprites for cave

---
+++ game/data/tilesets/castle/torch.lua
@@ -0,0 +1,206 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  name = "torch",
+  class = "",
+  tilewidth = 32,
+  tileheight = 32,
+  spacing = 0,
+  margin = 0,
+  columns = 4,
+  image = "torch.png",
+  imagewidth = 128,
+  imageheight = 64,
+  objectalignment = "center",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 32,
+    height = 32
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 8,
+  tiles = {
+    {
+      id = 0,
+      animation = {
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 1,
+      animation = {
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 2,
+      animation = {
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 3,
+      animation = {
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 4,
+      animation = {
+        {
+          tileid = 4,
+          duration = 50
+        },
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        }
+      }
+    },
+    {
+      id = 5,
+      animation = {
+        {
+          tileid = 5,
+          duration = 50
+        },
+        {
+          tileid = 0,
+          duration = 50
+        },
+        {
+          tileid = 1,
+          duration = 50
+        },
+        {
+          tileid = 2,
+          duration = 50
+        },
+        {
+          tileid = 3,
+          duration = 50
+        },
+        {
+          tileid = 4,
+          duration = 50
+        }
+      }
+    }
+  }
+}
---
+++ game/data/tilesets/castle/torch.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="torch" tilewidth="32" tileheight="32" tilecount="8" columns="4" objectalignment="center">
+ <editorsettings>
+  <export target="torch.lua" format="lua"/>
+ </editorsettings>
+ <image source="torch.png" width="128" height="64"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="0" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="50"/>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="50"/>
+   <frame tileid="0" duration="50"/>
+   <frame tileid="1" duration="50"/>
+   <frame tileid="2" duration="50"/>
+   <frame tileid="3" duration="50"/>
+   <frame tileid="4" duration="50"/>
+  </animation>
+ </tile>
+</tileset>
