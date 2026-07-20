5cc852935d7f86e8d3c9cca58e2bda9f3144aaf4
IoriBranford <ioribranford@gmail.com>
2026-07-20 11:45:16 -0700

    Castle decoratives for cave area

---
+++ game/data/tilesets/castle/castle_decoratives.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  name = "castle_decoratives",
+  class = "",
+  tilewidth = 32,
+  tileheight = 32,
+  spacing = 0,
+  margin = 0,
+  columns = 16,
+  image = "castle_decoratives.png",
+  imagewidth = 512,
+  imageheight = 512,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 32,
+    height = 32
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 256,
+  tiles = {}
+}
---
+++ game/data/tilesets/castle/castle_decoratives.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="castle_decoratives" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <editorsettings>
+  <export target="castle_decoratives.lua" format="lua"/>
+ </editorsettings>
+ <image source="castle_decoratives.png" width="512" height="512"/>
+</tileset>
