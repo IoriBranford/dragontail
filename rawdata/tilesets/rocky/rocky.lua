81c9462f2b8a1f516688fb9637c957ac9eef7728
IoriBranford <ioribranford@gmail.com>
2026-07-20 11:35:51 -0700

    Add Szadi Art RPGW mountains for later

---
+++ rawdata/tilesets/rocky/rocky.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  name = "rocky",
+  class = "",
+  tilewidth = 32,
+  tileheight = 32,
+  spacing = 0,
+  margin = 0,
+  columns = 42,
+  image = "rocky.png",
+  imagewidth = 1344,
+  imageheight = 2112,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 32,
+    height = 32
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 2772,
+  tiles = {}
+}
---
+++ rawdata/tilesets/rocky/rocky.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="rocky" tilewidth="32" tileheight="32" tilecount="2772" columns="42">
+ <editorsettings>
+  <export target="rocky.lua" format="lua"/>
+ </editorsettings>
+ <image source="rocky.png" width="1344" height="2112"/>
+</tileset>
