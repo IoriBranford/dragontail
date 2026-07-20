76e604b8987707dc3571fa309aee35e24a8139b9
IoriBranford <ioribranford@gmail.com>
2026-07-19 23:54:47 -0700

    Export Rose tileset to Lua

---
+++ game/data/sprites/player/rose/rose-main.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  name = "rose-main",
+  class = "",
+  tilewidth = 128,
+  tileheight = 128,
+  spacing = 0,
+  margin = 0,
+  columns = 1,
+  image = "main.ase",
+  imagewidth = 128,
+  imageheight = 128,
+  objectalignment = "center",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 80,
+    height = 80
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 1,
+  tiles = {}
+}
--- game/data/sprites/player/rose/rose-main.tsx
+++ game/data/sprites/player/rose/rose-main.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.11" tiledversion="1.11.2" name="rose-main" tilewidth="80" tileheight="80" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="24"/>
- <image source="main.ase" width="80" height="80"/>
+<tileset version="1.11" tiledversion="1.12.2" name="rose-main" tilewidth="128" tileheight="128" tilecount="1" columns="1" objectalignment="center">
+ <editorsettings>
+  <export target="rose-main.lua" format="lua"/>
+ </editorsettings>
+ <grid orientation="orthogonal" width="80" height="80"/>
+ <image source="main.ase" width="128" height="128"/>
 </tileset>
