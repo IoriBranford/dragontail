5fb182c1accd5f1c8000d8500f0a51f1db53c41d
IoriBranford <ioribranford@gmail.com>
2026-07-20 17:53:09 -0700

    Implement dodge dust

--- game/code/Dragontail/Character/Behavior/Fighter/DodgeLinear.lua
+++ game/code/Dragontail/Character/Behavior/Fighter/DodgeLinear.lua
@@ -2,34 +2,53 @@ local Face = require "Dragontail.Character.Component.Face"
 local Slide= require "Dragontail.Character.Component.Slide"
 local Body = require "Dragontail.Character.Component.Body"
 local Behavior = require "Dragontail.Character.Behavior"
+local Characters = require "Dragontail.Stage.Characters"
 
 local DodgeLinear = pooledclass(Behavior)
 DodgeLinear._nrec = Behavior._nrec + 3
 
 function DodgeLinear:start(dodgeangle)
-    local fighter = self.character
-    local opponent = fighter.opponents[1]
-    local x, y, oppox, oppoy = fighter.x, fighter.y, opponent.x, opponent.y
+    local ftr = self.character
+    local opponent = ftr.opponents[1]
+    local x, y, oppox, oppoy = ftr.x, ftr.y, opponent.x, opponent.y
     local tooppox, tooppoy = oppox - x, oppoy - y
     if tooppox == 0 and tooppoy == 0 then
         tooppox = 1
     end
     tooppox, tooppoy = math.norm(tooppox, tooppoy)
-    Face.faceVector(fighter, tooppox, tooppoy)
+    Face.faceVector(ftr, tooppox, tooppoy)
     self.opponent = opponent
     self.dodgeangle = dodgeangle
-    self.speed = fighter.dodgespeed or 1
-    self.speed = Slide.updateSlideSpeed(fighter, self.dodgeangle, self.speed, (fighter.accel or 1))
+    self.speed = ftr.dodgespeed or 1
+    self.speed = Slide.updateSlideSpeed(ftr, self.dodgeangle, self.speed, (ftr.accel or 1))
+    ftr.statetime = ftr.statetime or 30
 end
 
 function DodgeLinear:fixedupdate()
-    local fighter = self.character
-    local newstate, a, b, c, d, e, f = fighter:duringDodge(self.opponent)
+    local ftr = self.character
+    local newstate, a, b, c, d, e, f = ftr:duringDodge(self.opponent)
     if newstate then
         return newstate, a, b, c, d, e, f
     end
-    self.speed = Slide.updateSlideSpeed(fighter, self.dodgeangle, self.speed, (fighter.accel or 1))
-    if not fighter.statetime then
+    self.speed = Slide.updateSlideSpeed(ftr, self.dodgeangle, self.speed, (ftr.accel or 1))
+    if ftr.z == ftr.floorz
+    and ftr.statetime > 20
+    and ftr.statetime % 3 == 0 then
+        local revangle = self.dodgeangle+math.pi
+        local dust = {
+            type="spark-dodge-dust",
+            x = ftr.x, y = ftr.y, z = ftr.z,
+            velz = 2,
+            faceangle = revangle, accel = 1,
+        }
+        dust.x, dust.y = math2.vadd(dust.x, dust.y,
+            math2.frompolar(self.dodgeangle, self.bodyradius))
+
+        dust.velx, dust.vely = math2.frompolar(revangle, self.speed/4)
+
+        Characters.spawn(dust)
+    end
+    if not ftr.statetime then
         if self.speed == 0 then
             return "stand"
         end
