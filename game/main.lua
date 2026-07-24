fc32e9607b50ec58df58c95e5d948717d48c8728
IoriBranford <ioribranford@gmail.com>
2026-07-23 20:32:17 -0700

    Mount source base directory at root

--- game/main.lua
+++ game/main.lua
@@ -1,2 +1,6 @@
-love.filesystem.setRequirePath("code/?.lua;code/?/init.lua;libraries/?.lua;libraries/?/init.lua;"..love.filesystem.getRequirePath())
+local FS = love.filesystem
+
+FS.setRequirePath("code/?.lua;code/?/init.lua;libraries/?.lua;libraries/?/init.lua;"..love.filesystem.getRequirePath())
+local sbd = FS.getSourceBaseDirectory()
+FS.mount(sbd, "/")
 require("System.Main")("Dragontail")
