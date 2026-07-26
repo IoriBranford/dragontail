8ad18ca331383172b7b525b3227bc5b3a7019a6b
IoriBranford <ioribranford@gmail.com>
2026-07-25 17:15:34 -0700

    Add linux appimage to make-and-package

--- make-and-package.sh
+++ make-and-package.sh
@@ -24,6 +24,8 @@ declare -A PLATFORM_CONTENTS
 declare -A PLATFORM_FULL
 PLATFORM_CONTENTS["linux"]="$GAME_TITLE_NOSPACE-x86_64"
 PLATFORM_FULL["linux"]="linux-x86_64"
+PLATFORM_CONTENTS["linux-appimage"]="$GAME_TITLE_NOSPACE-x86_64.AppImage"
+PLATFORM_FULL["linux-appimage"]="linux-x86_64-appimage"
 PLATFORM_CONTENTS["windows"]="$GAME_TITLE"
 PLATFORM_FULL["windows"]="win-64"
 # PLATFORM_CONTENTS["game"]="${GAME_ASSET:="$PROJECT.love"} $CCDATA"
@@ -32,7 +34,10 @@ PLATFORM_FULL["game"]="data"
 for PLATFORM in ${*:2}
 do
     # echo $PLATFORM
-    ./make-${PLATFORM}.sh
+    if [ -e make-${PLATFORM}.sh ]
+    then
+        ./make-${PLATFORM}.sh
+    fi
     
     FULL_PLATFORM=${PLATFORM_FULL[$PLATFORM]}
     ZIP="$PROJECT-$VERSION-$FULL_PLATFORM.zip"
