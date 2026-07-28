68c3f834dd8de5d96ca13eccee7bba98d68760c9
IoriBranford <ioribranford@gmail.com>
2026-07-28 10:59:34 -0700

    Set version number in hud

--- game/code/Dragontail/Gui.lua
+++ game/code/Dragontail/Gui.lua
@@ -15,5 +15,14 @@ map:indexTilesetTilesByName()
 
 local gui = Gui.new(map)
 -- gui.options.screen.fullscreendevice.max = love.window.getDisplayCount()
+local FS = love.filesystem
+if FS.getInfo("version") then
+    local v = FS.read("version")
+    local ver = gui:get("gameplay.hud.development ver")
+    if ver and ver.text then
+        ver.text = string.gsub(ver.text, "0.0.0", v)
+    end
+end
+
 
 return gui
