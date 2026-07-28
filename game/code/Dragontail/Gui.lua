005ba1b65adee6285565bb8d2a2614ed8ed35795
IoriBranford <ioribranford@gmail.com>
2026-07-28 11:15:25 -0700

    Strip whitespace from version string

--- game/code/Dragontail/Gui.lua
+++ game/code/Dragontail/Gui.lua
@@ -17,7 +17,7 @@ local gui = Gui.new(map)
 -- gui.options.screen.fullscreendevice.max = love.window.getDisplayCount()
 local FS = love.filesystem
 if FS.getInfo("version") then
-    local v = FS.read("version")
+    local v = FS.read("version"):match("%S+")
     local ver = gui:get("gameplay.hud.development ver")
     if ver and ver.text then
         ver.text = string.gsub(ver.text, "0.0.0", v)
