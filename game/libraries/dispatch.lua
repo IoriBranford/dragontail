689268e2605c3d96990978ad917f42b85fdf116a
IoriBranford <ioribranford@gmail.com>
2026-07-28 18:39:43 -0700

    Updated dispatch

--- game/libraries/dispatch.lua
+++ game/libraries/dispatch.lua
@@ -1,122 +1,159 @@
-local setmt = setmetatable
+local tnew = require "table.new"
+
+---@alias listener table
 
 ---@class listeners
----@field [integer] table<string,function>
----@field free table<integer,true>
+---@field [integer] listener|false
+---@field free integer[]?
 
 ---@class dispatch
----@field [string] listeners
+---@field events table<string, listeners>
 local dispatch = {}
+dispatch.__index = dispatch
 
-function dispatch.new()
-    return setmt({}, dispatch)
+function dispatch.new(...)
+    local self = tnew(0, 1) ---@type dispatch
+    self.events = tnew(0, 16)
+    setmetatable(self, dispatch)
+    self:newevents(...)
+    return self
 end
 
-function dispatch:addEvent(ev)
-    local lsts = self[ev] or {free = {}}
-    self[ev] = lsts
-    return lsts
+function dispatch:newevent(ev)
+    if not self.events[ev] then
+        self.events[ev] = tnew(8, 1)
+    end
 end
 
-function dispatch:sub(ev, l)
-    local lsts = self:addEvent(ev)
-    local free = lsts.free
-    local i = free and free[#free] or (#lsts+1)
-    free[#free] = nil
-    lsts[i] = l
+function dispatch:newevents(...)
+    for i = 1, select("#", ...) do
+        local ev = select(i, ...)
+        self:newevent(ev)
+    end
+end
+
+---Subscribe
+---@param ev string
+---@param l listener
+---@return integer i
+function dispatch:sub(ev, l, after)
+    assert(type(l[ev]) == "function")
+
+    self:newevent(ev)
+    local ls = self.events[ev]
+
+    local free = ls.free
+    local i = not after and
+        free and free[#free]
+        or (#ls+1)
+    if free then free[#free] = nil end
+
+    ls[i] = l
     return i
 end
 
-function dispatch:unsub(ev, i)
-    local lsts = self[ev]
-    if lsts then
-        local free = lsts.free or {}
-        lsts.free = free
-        free[#free+1] = i
+function dispatch:allsub(l, after, force)
+    local sub = self.sub
+    if force then
+        for ev, f in pairs(l) do
+            if type(f) == "function" then
+                l[ev.."sub"] = sub(self, ev, l, after)
+            end
+        end
+    else
+        local evs = self.events
+        for ev, f in pairs(l) do
+            if evs[ev] and type(f) == "function" then
+                l[ev.."sub"] = sub(self, ev, l, after)
+            end
+        end
     end
 end
 
-function dispatch:sortSubs(ev, comp)
-    local lsts = self[ev]
-    if lsts then table.sort(lsts, comp) end
+---Unsubscribe
+---@param ev string
+---@param i integer
+---@param l listener?
+function dispatch:unsub(ev, i, l)
+    local ls = self.events[ev]
+    if not ls then return end
+
+    if l then assert(l == ls[i]) end
+
+    if i == #ls then ls[i] = nil return end
+
+    local free = ls.free or tnew(8, 0)
+    ls.free = free
+    free[#free+1] = i
+    ls[i] = false
 end
 
-local function event(lsts, ev, i1, i2, di, ...)
+function dispatch:allunsub(l)
+    local unsub = self.unsub
+    for ev in pairs(l) do
+        local i = l[ev.."sub"]
+        if i then
+            unsub(self, ev, i, l)
+            l[ev.."sub"] = nil
+        end
+    end
+end
+
+function dispatch:clearev(ev)
+    self.events[ev] = nil
+end
+
+local function send(ls, i1, i2, di, ev, ...)
     for i = i1, i2, di do
-        lsts[i][ev](...)
+        local l = ls[i]
+        if l then l[ev](...) end
     end
 end
 
-function dispatch:eventUp(ev, ...)
-    local lsts = self[ev]
-    if lsts then
-        event(lsts, ev, 1, #lsts, 1, ...)
-    end
-end
-
-function dispatch:eventDown(ev, ...)
-    local lsts = self[ev]
-    if lsts then
-        event(lsts, ev, #lsts, 1, -1, ...)
-    end
-end
-
-local function eventMut(lsts, ev, i1, i2, di, a, b, c, d, e, f)
+local function sendself(ls, i1, i2, di, ev, ...)
     for i = i1, i2, di do
-        local u, v, w, x, y, z
-            = lsts[i][ev](a, b, c, d, e, f)
-        if u ~= nil then a = u end
-        if v ~= nil then b = v end
-        if w ~= nil then c = w end
-        if x ~= nil then d = x end
-        if y ~= nil then e = y end
-        if z ~= nil then f = z end
-    end
-    return a, b, c, d, e, f
-end
-
-
-function dispatch:eventUpMut(ev, a, b, c, d, e, f)
-    local lsts = self[ev]
-    if lsts then
-        eventMut(lsts, ev, 1, #lsts, 1, a, b, c, d, e, f)
+        local l = ls[i]
+        if l then l[ev](l, ...) end
     end
 end
 
-function dispatch:eventDownMut(ev, a, b, c, d, e, f)
-    local lsts = self[ev]
-    if lsts then
-        eventMut(lsts, ev, #lsts, 1, -1, a, b, c, d, e, f)
-    end
+function dispatch:send(ev, ...)
+    local ls = self.events[ev]
+    if ls then send(ls, 1, #ls, 1, ev, ...) end
 end
 
-local callbacks = {}
-
-local function eventCB(lsts, ev, i1, i2, di, ...)
-    if not lsts then return end
-    local cbs = callbacks
-    for i = i1, i2, di do
-        cbs[#cbs+1] = lsts[i][ev]()
-    end
-    for i = #cbs, 1, -1 do
-        local cb = cbs[i]
-        cbs[#cbs] = nil
-        cb()
-    end
+function dispatch:rsend(ev, ...)
+    local ls = self.events[ev]
+    if ls then send(ls, #ls, 1, -1, ev, ...) end
 end
 
-function dispatch:eventUpCB(ev, ...)
-    local lsts = self[ev]
-    if lsts then
-        eventCB(lsts, ev, 1, #lsts, 1, ...)
-    end
+function dispatch:sendself(ev, ...)
+    local ls = self.events[ev]
+    if ls then sendself(ls, 1, #ls, 1, ev, ...) end
 end
 
-function dispatch:eventDownCB(ev, ...)
-    local lsts = self[ev]
-    if lsts then
-        eventCB(self[ev], ev, #lsts, 1, -1, ...)
+function dispatch:rsendself(ev, ...)
+    local ls = self.events[ev]
+    if ls then sendself(ls, #ls, 1, -1, ev, ...) end
+end
+
+function dispatch:sort(ev, cmp)
+    local ls = self.events[ev]
+    if not ls then return end
+
+    table.sort(ls, function(a, b)
+        return a and not b
+            or a and b and cmp(a, b)
+    end)
+
+    while not ls[#ls] do
+        ls[#ls] = nil
     end
+    local sub = ev.."sub"
+    for i = 1, #ls do
+        ls[i][sub] = i
+    end
+    ls.free = nil
 end
 
 return dispatch
