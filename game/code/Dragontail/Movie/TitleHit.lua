0da468cc075512b3ee0ef0605158f5031ea4330c
IoriBranford <ioribranford@gmail.com>
2026-07-29 22:33:04 -0700

    Use the separate title screen and wipe maps

---
+++ game/code/Dragontail/Movie/TitleHit.lua
@@ -0,0 +1,60 @@
+local coRelativePath = require "Dragontail.Movie.coRelativePath"
+local coHitShake     = require "Dragontail.Movie.coHitShake"
+local coFade         = require "Dragontail.Movie.coFade"
+local Color          = require "Tiled.Color"
+local multitask      = require "multitask"
+
+local wrap = coroutine.wrap
+local yield = coroutine.yield
+
+local function TitleHit(layers)
+    local fg = layers.fg
+    local directions = layers.directions
+    directions.visible = false
+    assert(directions and directions.type == "objectgroup")
+    ---@cast directions ObjectGroup
+
+    local path = directions.path
+    coRelativePath(path, fg, 50)
+
+    local function coDrift(obj, a, s, t)
+        local dx, dy = math2.frompolar(a)
+        local ds = s/t
+        for _ = 1, t do
+            yield()
+            obj.x = obj.x + dx*s
+            obj.y = obj.y + dy*s
+            s = s - ds
+        end
+        return true
+    end
+
+    yield("hit")
+
+    local shaking = directions.shaking
+    local rose = fg.Rose
+    local swing = fg.swing
+    local enemy = fg.enemy
+    local hit = fg.hit
+    local tasks = {
+        function() return coDrift(rose, math.rad(210), .5, 60) end,
+        function() return coDrift(swing, math.rad(210), .5, 60) end,
+        function() return coDrift(enemy, math.rad(210), 2, 60) end,
+        function() return coDrift(hit, math.rad(210), 1, 60) end,
+        function() coroutine.wait(30); return coFade(hit, 0x80ffffff, Color.White, 30) end,
+        function() coroutine.wait(30); return coFade(swing, 0x80ffffff, Color.White, 30) end,
+        function() return coHitShake(shaking, 100, 50, 150, 60) end,
+    }
+
+    local mt = multitask.new()
+    for i = 1, #tasks do
+        mt:push(wrap(tasks[i]))
+    end
+    repeat
+        mt:runAll()
+        yield()
+    until mt:allDone()
+    return true
+end
+
+return TitleHit
--- game/code/Dragontail/TitlePhase.lua
+++ game/code/Dragontail/TitlePhase.lua
@@ -1,134 +1,65 @@
-local Gui = require "Dragontail.Gui"
-local GuiActions = require "Dragontail.GuiActions"
 -- local Wallpaper = require "System.Wallpaper"
 local Assets    = require "Tiled.Assets"
-local Config = require "System.Config"
-local Canvas = require "System.Canvas"
-local Tiled  = require "Tiled"
-local Path   = require "Object.Path"
 local Audio  = require "System.Audio"
-local Dragontail = require "Dragontail"
-local coRelativePath = require "Dragontail.Movie.coRelativePath"
-local coHitShake     = require "Dragontail.Movie.coHitShake"
-local coFade         = require "Dragontail.Movie.coFade"
-local Color          = require "Tiled.Color"
-local multitask      = require "multitask"
+local Gui            = require "Gui"
+local TitleHit       = require "Dragontail.Movie.TitleHit"
 local TitlePhase = {}
 
-local scenemap ---@type TiledMap
+local scenemap ---@type Gui
 local sceneco ---@type function
 local ambientsound ---@type love.Source
 
-local wrap = coroutine.wrap
-local yield = coroutine.yield
-
-local function sceneAnimation()
-    local layers = scenemap.layers
-    local fg = layers.fg
-    local directions = layers.directions
-    layers.logo.visible = false
-    assert(directions and directions.type == "objectgroup")
-    ---@cast directions ObjectGroup
-    local path = directions.path
-    ---@cast path Path
-    Path.cast(path)
-    coRelativePath(path, fg, 50)
-    if ambientsound and Audio.getMusicVolume() > 0 then
-        ambientsound:stop()
-    end
-
-    local menu = assert(Gui.title.mainmenus.normal)
-    menu:setVisible(true)
-
-    local function coDrift(obj, a, s, t)
-        local dx, dy = math2.frompolar(a)
-        local ds = s/t
-        for _ = 1, t do
-            yield()
-            obj.x = obj.x + dx*s
-            obj.y = obj.y + dy*s
-            s = s - ds
-        end
-        return true
-    end
-
-    local rose = fg.Rose
-    local swing = fg.swing
-    local enemy = fg.enemy
-    local hit = fg.hit
-    local tasks = {
-        function() return coDrift(rose, math.rad(210), .5, 60) end,
-        function() return coDrift(swing, math.rad(210), .5, 60) end,
-        function() return coDrift(enemy, math.rad(210), 2, 60) end,
-        function() return coDrift(hit, math.rad(210), 1, 60) end,
-        function() coroutine.wait(30); return coFade(hit, 0x80ffffff, Color.White, 30) end,
-        function() coroutine.wait(30); return coFade(swing, 0x80ffffff, Color.White, 30) end,
-        function() return coHitShake(menu, 100, 50, 150, 60) end,
-    }
-
-    local mt = multitask.new()
-    for i = 1, #tasks do
-        mt:push(wrap(tasks[i]))
-    end
-    repeat
-        mt:runAll()
-        yield()
-    until mt:allDone()
-
-    Gui:pushMenu(menu)
-    return true
-end
-
 function TitlePhase.loadphase(startwithmainmenu)
-    scenemap = Tiled.Map.load("data/title_scene.lua")
-    scenemap:indexLayersByName()
-    scenemap:indexLayerObjectsByName()
-    scenemap:bindClasses()
-    scenemap.layers:showOnlyNamed("bg", "logo")
+    scenemap = Gui.new("data/gui/screen_title.lua")
+    scenemap:showOnlyNamed("bg", "title", "etc")
+    love.event.connectAll(scenemap)
+
+    local wipemap = Gui.new("data/gui/wipe_diagonalcurtains.lua")
+    local wipe = wipemap.wipe ---@cast wipe Wipe
+    wipe:start("open")
+    love.event.connectAll(wipemap)
+
+    Assets.get("ccdata/music/Block Island Sound loop.ogg")
 
     Assets.uncacheMarked()
     Assets.packTiles()
     Assets.batchAllMapsLayers()
 
-    Gui:showOnlyNamed("title", "options", "wipe")
-    Gui:clearMenuStack()
-    Gui.title:showOnlyNamed("title")
-    Gui.options:showOnlyNamed()
-    -- Wallpaper.reload()
-    Assets.get("ccdata/music/Block Island Sound loop.ogg")
-
-    local wipe = Gui.wipe.diagonalCurtains ---@cast wipe Wipe
-    wipe:start("open")
     if startwithmainmenu then
         TitlePhase.pushMainMenu()
     else
-        Gui:pushMenu(Gui.title.pressstart)
+        scenemap:pushMenu(scenemap.title)
         ambientsound = Audio.play("data/sounds/ambient/seaside.ogg")
         if ambientsound then ambientsound:setLooping(true) end
     end
-    scenemap:eventconnect()
-    love.event.connectAll(scenemap)
-    love.event.connectAll(Gui)
 end
 
 function TitlePhase.pushMainMenu()
-    Gui.title.pressstart:setVisible(false)
-    sceneco = coroutine.wrap(sceneAnimation)
+    scenemap:showOnlyNamed("bg", "etc", "TitleHit")
+    sceneco = coroutine.wrap(function ()
+        local menu = scenemap.menu
+        local shaking = scenemap.TitleHit.directions.shaking
+        local titlehit = coroutine.wrap(TitleHit)
+        local ok, status = pcall(titlehit, scenemap.TitleHit)
+        while ok and status ~= true do
+            coroutine.yield()
+            ok, status = pcall(titlehit)
+            if status == "hit" then
+                menu.visible = true
+            end
+            menu.x, menu.y = shaking.x, shaking.y
+        end
+        if not ok then print(status) end
+        scenemap:pushMenu(menu)
+        return true
+    end)
     sceneco()
-    scenemap.layers:showOnlyNamed("bg", "fg")
-    local menuname = "normal"
-    if Config.exhibit then
-        menuname = "exhibit"
-    end
-    Gui.title.mainmenus:setVisible(true)
-    Gui.title.mainmenus:showOnlyNamed()
     Audio.playMusic("ccdata/music/Block Island Sound loop.ogg", nil, true)
 end
 
 function TitlePhase.quitphase()
     Audio.stop()
     Assets.markAllToUncache()
-    Gui:clearMenuStack()
     scenemap = nil
     sceneco = nil
     ambientsound = nil
