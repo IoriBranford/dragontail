b3fe7f35851a9d3c75f9480af722834debcf7e8b
IoriBranford <ioribranford@gmail.com>
2026-08-03 08:44:26 -0700

    Palm tree 2a and 2b objects and graphics

---
+++ game/data/sprites/sandy/tree2a-crown.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="tree2a-crown" tilewidth="64" tileheight="48" tilecount="1" columns="1" objectalignment="bottom">
+ <editorsettings>
+  <export target="tree2a-crown.lua" format="lua"/>
+ </editorsettings>
+ <tileoffset x="0" y="8"/>
+ <image source="tree2a-crown.ase" width="64" height="48"/>
+</tileset>
---
+++ game/data/sprites/sandy/tree2a-trunk.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  name = "tree2a-trunk",
+  class = "",
+  tilewidth = 64,
+  tileheight = 48,
+  spacing = 0,
+  margin = 0,
+  columns = 1,
+  image = "tree2a-trunk.ase",
+  imagewidth = 64,
+  imageheight = 48,
+  objectalignment = "bottom",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 8
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 64,
+    height = 48
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 1,
+  tiles = {}
+}
