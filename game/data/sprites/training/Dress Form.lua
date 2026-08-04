c1ebf2fdec12e20ce2e5a64d19702cf530661f31
IoriBranford <ioribranford@gmail.com>
2026-08-04 11:41:03 -0700

    Training target assets and data

---
+++ game/data/sprites/training/Dress Form.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  name = "Dress Form",
+  class = "",
+  tilewidth = 32,
+  tileheight = 32,
+  spacing = 0,
+  margin = 0,
+  columns = 6,
+  image = "Dress Form.png",
+  imagewidth = 192,
+  imageheight = 64,
+  objectalignment = "bottom",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 4
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 32,
+    height = 32
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 12,
+  tiles = {}
+}
---
+++ game/data/sprites/training/Dress Form.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="Dress Form" tilewidth="32" tileheight="32" tilecount="12" columns="6" objectalignment="bottom">
+ <editorsettings>
+  <export target="Dress Form.lua" format="lua"/>
+ </editorsettings>
+ <tileoffset x="0" y="4"/>
+ <image source="Dress Form.png" width="192" height="64"/>
+</tileset>
