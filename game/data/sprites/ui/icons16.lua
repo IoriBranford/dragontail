abc83ab72529c7c32649ceb59090eb1bfeb42d93
IoriBranford <ioribranford@gmail.com>
2026-08-11 19:26:52 -0700

    Start 16x16 icon tileset

---
+++ game/data/sprites/ui/icons16.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.12.2",
+  name = "icons16",
+  class = "",
+  tilewidth = 16,
+  tileheight = 16,
+  spacing = 0,
+  margin = 0,
+  columns = 2,
+  image = "icons16.png",
+  imagewidth = 32,
+  imageheight = 16,
+  objectalignment = "center",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 16,
+    height = 16
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 2,
+  tiles = {}
+}
---
+++ game/data/sprites/ui/icons16.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.12.2" name="icons16" tilewidth="16" tileheight="16" tilecount="2" columns="2" objectalignment="center">
+ <editorsettings>
+  <export target="icons16.lua" format="lua"/>
+ </editorsettings>
+ <image source="icons16.png" width="32" height="16"/>
+</tileset>
